22f726a6debbb166cfd5a529ed9fba803abba09f
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-01-20 19:47:35 -0800

    Finished with HW1 (basic checkin)

--- Mickey.Phoenix/Homework1Solution/Homework1Solution/Form1.Designer.cs
+++ Mickey.Phoenix/Homework1Solution/Homework1Solution/Form1.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(61)))), ((int)(((byte)(159)))));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "...because doing homework helps me learn better.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(743, 132);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "I love doing homework...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
