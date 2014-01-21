6e1fa0f604b10479246b0d646c9401f681af55d4
ivarmlee11 <ivarr5@gmail.com>
2014-01-21 14:30:50 -0800

    HW 1 With Message Box

--- Ivar.Lee/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ Ivar.Lee/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.doSomething = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "I made a label.";
             // 
+            // doSomething
+            // 
+            this.doSomething.Location = new System.Drawing.Point(53, 106);
+            this.doSomething.Name = "doSomething";
+            this.doSomething.Size = new System.Drawing.Size(218, 86);
+            this.doSomething.TabIndex = 1;
+            this.doSomething.Text = "Important Message Generator";
+            this.doSomething.UseVisualStyleBackColor = true;
+            this.doSomething.Click += new System.EventHandler(this.doSomething_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(764, 455);
+            this.Controls.Add(this.doSomething);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "This is a bit of homework.";
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button doSomething;
     }
 }
 
--- Ivar.Lee/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Ivar.Lee/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void doSomething_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(@"IMPORTANT MESSAGE");
+        }
     }
 }
