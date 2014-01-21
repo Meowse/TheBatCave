225295c7d96ff1c7af099f70fecb85c82072f6d2
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-01-20 19:48:30 -0800

    Cleaned up UI presentation.

--- Mickey.Phoenix/Homework1Solution/Homework1Solution/Form1.Designer.cs
+++ Mickey.Phoenix/Homework1Solution/Homework1Solution/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(743, 132);
+            this.ClientSize = new System.Drawing.Size(433, 51);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "I love doing homework...";
