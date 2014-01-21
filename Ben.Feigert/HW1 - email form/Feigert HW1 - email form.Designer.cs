8cff6cc3fd251b85ddb05c64f7afdd8c80444d35
bfeigert <bfeigert@gmail.com>
2014-01-21 13:32:23 -0700

    hw 1 - email form

---
+++ Ben.Feigert/HW1 - email form/Feigert HW1 - email form.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textboxFname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxLname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPwd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxIceCream = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxMagic = new System.Windows.Forms.TextBox();
+            this.labelMagic = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textboxFname
+            // 
+            this.textboxFname.Location = new System.Drawing.Point(127, 133);
+            this.textboxFname.Name = "textboxFname";
+            this.textboxFname.Size = new System.Drawing.Size(177, 20);
+            this.textboxFname.TabIndex = 0;
+            this.textboxFname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last Name";
+            // 
+            // textBoxLname
+            // 
+            this.textBoxLname.Location = new System.Drawing.Point(127, 168);
+            this.textBoxLname.Name = "textBoxLname";
+            this.textBoxLname.Size = new System.Drawing.Size(177, 20);
+            this.textBoxLname.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Username";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(127, 203);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(177, 20);
+            this.textBoxUsername.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email";
+            // 
+            // textBoxPwd
+            // 
+            this.textBoxPwd.Location = new System.Drawing.Point(127, 238);
+            this.textBoxPwd.Name = "textBoxPwd";
+            this.textBoxPwd.Size = new System.Drawing.Size(177, 20);
+            this.textBoxPwd.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(127, 308);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(177, 20);
+            this.textBoxEmail.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 39);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Ice cream flavor";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBoxIceCream
+            // 
+            this.textBoxIceCream.Location = new System.Drawing.Point(127, 273);
+            this.textBoxIceCream.Name = "textBoxIceCream";
+            this.textBoxIceCream.Size = new System.Drawing.Size(177, 20);
+            this.textBoxIceCream.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(38, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(235, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Ice Cream Delivery";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(38, 49);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(257, 25);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Account Registration";
+            // 
+            // textBoxMagic
+            // 
+            this.textBoxMagic.Location = new System.Drawing.Point(252, 353);
+            this.textBoxMagic.Name = "textBoxMagic";
+            this.textBoxMagic.Size = new System.Drawing.Size(52, 20);
+            this.textBoxMagic.TabIndex = 6;
+            // 
+            // labelMagic
+            // 
+            this.labelMagic.AutoSize = true;
+            this.labelMagic.Location = new System.Drawing.Point(40, 353);
+            this.labelMagic.Name = "labelMagic";
+            this.labelMagic.Size = new System.Drawing.Size(179, 13);
+            this.labelMagic.TabIndex = 16;
+            this.labelMagic.Text = "Magic code (Ben\'s gmail credentials)";
+            this.labelMagic.Click += new System.EventHandler(this.labelMagic_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(40, 92);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(291, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Please fill out this form. We\'ll send you an email confirmation.";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(362, 456);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.labelMagic);
+            this.Controls.Add(this.textBoxMagic);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxIceCream);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPwd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxLname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textboxFname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textboxFname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxLname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPwd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxIceCream;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxMagic;
+        private System.Windows.Forms.Label labelMagic;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
