be76373e3472dc2f1e5e905e652566bc33d6576b
sajohn84 <johnson.sara.j@hotmail.com>
2014-01-20 22:40:47 -0800

    HW 1 Check-in
    Checking in my changes.

--- Sara.Johnson/NotJustHelloWorld/NotJustHelloWorld/Form1.Designer.cs
+++ Sara.Johnson/NotJustHelloWorld/NotJustHelloWorld/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click Me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "My Favorite Number Is:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(41, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(223, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(223, 234);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(223, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 52);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Move The Text";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 367);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Sara.Johnson/NotJustHelloWorld/NotJustHelloWorld/Form1.cs
+++ Sara.Johnson/NotJustHelloWorld/NotJustHelloWorld/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace NotJustHelloWorld
 {
     public partial class Form1 : Form
@@ -16,5 +18,56 @@ namespace NotJustHelloWorld
         {
             InitializeComponent();
         }
+
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("You Clicked Me!");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Probably 4");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //string MyString = textBox1.Text;
+
+
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = textBox1.Text;
+                textBox1.Clear();
+            }
+            else
+            {
+                if (textBox1.Text == "")
+                {
+                    textBox1.Text = textBox2.Text;
+                    textBox2.Clear();
+                }
+            }
+
+        }
+
     }
+
+
 }
