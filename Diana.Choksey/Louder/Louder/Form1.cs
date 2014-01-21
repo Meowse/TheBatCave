c213a7133339ce2ba89cf0dcab5c9a93883a3777
Diana Choksey <d_choksey@hotmail.com>
2014-01-21 10:46:36 -0800

    HW 1 completed

--- Diana.Choksey/Louder/Louder/Form1.cs
+++ Diana.Choksey/Louder/Louder/Form1.cs
@@ -16,5 +16,30 @@ namespace Louder
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+	        MessageBox.Show("You're not loud enough, I can't hear you!");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("What you say? You're still not loud enough, I can't hear you!");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You just set a world record for being 110 decibels loud! Way to go!");
+        }
     }
 }
