a14534b96ce71b883a04878b4440524fc16c4f44
dulcinealedgerwood <dulcinealedgerwood>
2014-01-21 21:04:56 -0800

    Lab2a again
    Lab 2a again

--- Dulcinea.Ledgerwood/Lab2a/Lab2a/Form1.Designer.cs
+++ Dulcinea.Ledgerwood/Lab2a/Lab2a/Form1.Designer.cs
@@ -49,152 +49,168 @@
             // output1
             // 
             this.output1.AutoSize = true;
-            this.output1.Location = new System.Drawing.Point(100, 9);
+            this.output1.Location = new System.Drawing.Point(150, 14);
+            this.output1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.output1.Name = "output1";
-            this.output1.Size = new System.Drawing.Size(96, 13);
+            this.output1.Size = new System.Drawing.Size(143, 20);
             this.output1.TabIndex = 3;
             this.output1.Text = "output 1 goes here";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.Size = new System.Drawing.Size(86, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Hello world";
             // 
             // output2
             // 
             this.output2.AutoSize = true;
-            this.output2.Location = new System.Drawing.Point(100, 33);
+            this.output2.Location = new System.Drawing.Point(150, 51);
+            this.output2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.output2.Name = "output2";
-            this.output2.Size = new System.Drawing.Size(96, 13);
+            this.output2.Size = new System.Drawing.Size(143, 20);
             this.output2.TabIndex = 5;
             this.output2.Text = "output 2 goes here";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 33);
+            this.label3.Location = new System.Drawing.Point(18, 51);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.Size = new System.Drawing.Size(106, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "one two three";
             // 
             // output3
             // 
             this.output3.AutoSize = true;
-            this.output3.Location = new System.Drawing.Point(100, 57);
+            this.output3.Location = new System.Drawing.Point(150, 88);
+            this.output3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.output3.Name = "output3";
-            this.output3.Size = new System.Drawing.Size(96, 13);
+            this.output3.Size = new System.Drawing.Size(143, 20);
             this.output3.TabIndex = 7;
             this.output3.Text = "output 3 goes here";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 58);
+            this.label5.Location = new System.Drawing.Point(18, 89);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.Size = new System.Drawing.Size(27, 20);
             this.label5.TabIndex = 6;
             this.label5.Text = "45";
             // 
             // output4
             // 
             this.output4.AutoSize = true;
-            this.output4.Location = new System.Drawing.Point(100, 81);
+            this.output4.Location = new System.Drawing.Point(150, 125);
+            this.output4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.output4.Name = "output4";
-            this.output4.Size = new System.Drawing.Size(96, 13);
+            this.output4.Size = new System.Drawing.Size(143, 20);
             this.output4.TabIndex = 9;
             this.output4.Text = "output 4 goes here";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 82);
+            this.label7.Location = new System.Drawing.Point(18, 126);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.Size = new System.Drawing.Size(27, 20);
             this.label7.TabIndex = 8;
             this.label7.Text = "\"\"\"";
             // 
             // output5
             // 
             this.output5.AutoSize = true;
-            this.output5.Location = new System.Drawing.Point(100, 105);
+            this.output5.Location = new System.Drawing.Point(150, 162);
+            this.output5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.output5.Name = "output5";
-            this.output5.Size = new System.Drawing.Size(96, 13);
+            this.output5.Size = new System.Drawing.Size(143, 20);
             this.output5.TabIndex = 11;
             this.output5.Text = "output 5 goes here";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(12, 106);
+            this.label9.Location = new System.Drawing.Point(18, 163);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.Size = new System.Drawing.Size(25, 20);
             this.label9.TabIndex = 10;
             this.label9.Text = "\\\\\\\\";
             // 
             // output6
             // 
             this.output6.AutoSize = true;
-            this.output6.Location = new System.Drawing.Point(100, 130);
+            this.output6.Location = new System.Drawing.Point(150, 200);
+            this.output6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.output6.Name = "output6";
-            this.output6.Size = new System.Drawing.Size(96, 13);
+            this.output6.Size = new System.Drawing.Size(143, 20);
             this.output6.TabIndex = 13;
             this.output6.Text = "output 6 goes here";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(12, 131);
+            this.label11.Location = new System.Drawing.Point(18, 202);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(27, 13);
+            this.label11.Size = new System.Drawing.Size(31, 20);
             this.label11.TabIndex = 12;
             this.label11.Text = "\"\\\"\"";
             // 
             // output7
             // 
             this.output7.AutoSize = true;
-            this.output7.Location = new System.Drawing.Point(100, 154);
+            this.output7.Location = new System.Drawing.Point(150, 237);
+            this.output7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.output7.Name = "output7";
-            this.output7.Size = new System.Drawing.Size(96, 13);
+            this.output7.Size = new System.Drawing.Size(143, 20);
             this.output7.TabIndex = 15;
             this.output7.Text = "output 7 goes here";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(12, 155);
+            this.label13.Location = new System.Drawing.Point(18, 238);
+            this.label13.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(67, 13);
+            this.label13.Size = new System.Drawing.Size(71, 20);
             this.label13.TabIndex = 14;
             this.label13.Text = "\"\\\"\"\"\\\\\\\"\\\"\"";
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(202, 33);
+            this.label16.Location = new System.Drawing.Point(303, 51);
+            this.label16.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(101, 13);
+            this.label16.Size = new System.Drawing.Size(155, 20);
             this.label16.TabIndex = 18;
             this.label16.Text = "(using string.Format)";
             // 
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(202, 57);
+            this.label17.Location = new System.Drawing.Point(303, 88);
+            this.label17.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(125, 13);
+            this.label17.Size = new System.Drawing.Size(187, 20);
             this.label17.TabIndex = 19;
             this.label17.Text = "(using some int variables)";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(355, 244);
+            this.ClientSize = new System.Drawing.Size(532, 375);
             this.Controls.Add(this.label17);
             this.Controls.Add(this.label16);
             this.Controls.Add(this.output7);
@@ -211,8 +227,10 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.output1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
