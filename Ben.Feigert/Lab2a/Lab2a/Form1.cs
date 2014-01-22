533f717d0ffaaf8a949bb99b287948b71f196d80
bfeigert <bfeigert@gmail.com>
2014-01-21 22:23:21 -0700

    Lab2a progress
    deleted click handlers and figured out how to output for each field.

--- Ben.Feigert/Lab2a/Lab2a/Form1.cs
+++ Ben.Feigert/Lab2a/Lab2a/Form1.cs
@@ -15,6 +15,29 @@ namespace Lab2a
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //number 1
+            output1.Text = ("Hello World");
+
+            //number 2
+            String numberTwo = ("one two three");
+            output2.Text = numberTwo;
+            
+            //number 3
+            int x = 9;
+            int y = 5;
+            int product = (x * y);
+
+            string strProduct = product.ToString();
+            output3.Text = strProduct;
+
+          
+
+
         }
     }
 }
