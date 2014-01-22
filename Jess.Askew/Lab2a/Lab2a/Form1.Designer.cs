a0bf7526f5438e6625a59d78c5618a558e572330
slightlyaskew <jessica.e.askew@gmail.com>
2014-01-21 20:33:52 -0800

    Lab2a baseline
    Not total baseline, but baseline when I remembered to save

---
+++ Jess.Askew/Lab2a/Lab2a/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Lab2a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBLHelloWorld = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LBLonetwothree = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LBLfortyfive = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LBLtwodblquote = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.LBLfourbackslash = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.LBLquoteslashtwoquote = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.BTNcodeify = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LBLHelloWorld
+            // 
+            this.LBLHelloWorld.AutoSize = true;
+            this.LBLHelloWorld.Location = new System.Drawing.Point(100, 9);
+            this.LBLHelloWorld.Name = "LBLHelloWorld";
+            this.LBLHelloWorld.Size = new System.Drawing.Size(96, 13);
+            this.LBLHelloWorld.TabIndex = 3;
+            this.LBLHelloWorld.Text = "output 1 goes here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hello world";
+            // 
+            // LBLonetwothree
+            // 
+            this.LBLonetwothree.AutoSize = true;
+            this.LBLonetwothree.Location = new System.Drawing.Point(100, 33);
+            this.LBLonetwothree.Name = "LBLonetwothree";
+            this.LBLonetwothree.Size = new System.Drawing.Size(96, 13);
+            this.LBLonetwothree.TabIndex = 5;
+            this.LBLonetwothree.Text = "output 2 goes here";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "one two three";
+            // 
+            // LBLfortyfive
+            // 
+            this.LBLfortyfive.AutoSize = true;
+            this.LBLfortyfive.Location = new System.Drawing.Point(100, 57);
+            this.LBLfortyfive.Name = "LBLfortyfive";
+            this.LBLfortyfive.Size = new System.Drawing.Size(96, 13);
+            this.LBLfortyfive.TabIndex = 7;
+            this.LBLfortyfive.Text = "output 3 goes here";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "45";
+            // 
+            // LBLtwodblquote
+            // 
+            this.LBLtwodblquote.AutoSize = true;
+            this.LBLtwodblquote.Location = new System.Drawing.Point(100, 81);
+            this.LBLtwodblquote.Name = "LBLtwodblquote";
+            this.LBLtwodblquote.Size = new System.Drawing.Size(96, 13);
+            this.LBLtwodblquote.TabIndex = 9;
+            this.LBLtwodblquote.Text = "output 4 goes here";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "\"\"\"";
+            // 
+            // LBLfourbackslash
+            // 
+            this.LBLfourbackslash.AutoSize = true;
+            this.LBLfourbackslash.Location = new System.Drawing.Point(100, 105);
+            this.LBLfourbackslash.Name = "LBLfourbackslash";
+            this.LBLfourbackslash.Size = new System.Drawing.Size(96, 13);
+            this.LBLfourbackslash.TabIndex = 11;
+            this.LBLfourbackslash.Text = "output 5 goes here";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 106);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "\\\\\\\\";
+            // 
+            // LBLquoteslashtwoquote
+            // 
+            this.LBLquoteslashtwoquote.AutoSize = true;
+            this.LBLquoteslashtwoquote.Location = new System.Drawing.Point(100, 130);
+            this.LBLquoteslashtwoquote.Name = "LBLquoteslashtwoquote";
+            this.LBLquoteslashtwoquote.Size = new System.Drawing.Size(96, 13);
+            this.LBLquoteslashtwoquote.TabIndex = 13;
+            this.LBLquoteslashtwoquote.Text = "output 6 goes here";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 131);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(27, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "\"\\\"\"";
+            // 
+            // LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote
+            // 
+            this.LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote.AutoSize = true;
+            this.LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote.Location = new System.Drawing.Point(100, 154);
+            this.LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote.Name = "LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote";
+            this.LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote.Size = new System.Drawing.Size(96, 13);
+            this.LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote.TabIndex = 15;
+            this.LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote.Text = "output 7 goes here";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 155);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(67, 13);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "\"\\\"\"\"\\\\\\\"\\\"\"";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(202, 33);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(101, 13);
+            this.label16.TabIndex = 18;
+            this.label16.Text = "(using string.Format)";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(202, 57);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(125, 13);
+            this.label17.TabIndex = 19;
+            this.label17.Text = "(using some int variables)";
+            // 
+            // BTNcodeify
+            // 
+            this.BTNcodeify.Location = new System.Drawing.Point(235, 174);
+            this.BTNcodeify.Name = "BTNcodeify";
+            this.BTNcodeify.Size = new System.Drawing.Size(75, 23);
+            this.BTNcodeify.TabIndex = 20;
+            this.BTNcodeify.Text = "codeify";
+            this.BTNcodeify.UseVisualStyleBackColor = true;
+            this.BTNcodeify.Click += new System.EventHandler(this.BTNcodeify_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 244);
+            this.Controls.Add(this.BTNcodeify);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.LBLquoteslashtwoquote);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.LBLfourbackslash);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.LBLtwodblquote);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.LBLfortyfive);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LBLonetwothree);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LBLHelloWorld);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBLHelloWorld;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LBLonetwothree;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LBLfortyfive;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LBLtwodblquote;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label LBLfourbackslash;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label LBLquoteslashtwoquote;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label LBLquoteslashtwoquotethreeslashthreequoteslashtwoquote;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Button BTNcodeify;
+    }
+}
+
