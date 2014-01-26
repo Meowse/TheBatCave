eacd2b50c54712b604c0346680653c9da8bfff34
ShaunaB <shauna.bennett@paccar.com>
2014-01-25 20:59:18 -0800

    Lab2bBaseline

---
+++ Shauna.Bennett/Session 2/Lab2b/Lab2b/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2b
+{
+    public class Person
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}
