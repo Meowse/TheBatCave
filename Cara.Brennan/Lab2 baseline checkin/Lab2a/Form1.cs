e5f1cc4ceb16cc30a1cfd8d93e22997bd882d9b3
cbrenn <cararbrennan@gmail.com>
2014-01-26 17:15:45 -0800

    Brennan Lab2a

--- Cara.Brennan/Lab2 baseline checkin/Lab2a/Form1.cs
+++ Cara.Brennan/Lab2 baseline checkin/Lab2a/Form1.cs
@@ -15,6 +15,31 @@ namespace Lab2a
         public Form1()
         {
             InitializeComponent();
+
+            String HelloWorld = "Hello World";
+            output1.Text = HelloWorld;
+
+            String onetwothree = string.Format("one two three");
+            output2.Text = onetwothree;
+
+            int s = 1;
+            int c = 40;
+            output3.Text = (s*c).ToString();
+
+           
+            output4.Text = "\"\"\"";
+
+            output5.Text = "\\\\\\\\";
+
+            
+            output6.Text = "\"\\\"\"";
+
+            output7.Text = "\"\\\"\"\"\\\\\\\"\\\"\"";
+
+
+
+
+
         }
     }
 }
