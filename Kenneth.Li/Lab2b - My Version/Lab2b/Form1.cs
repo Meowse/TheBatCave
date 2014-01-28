23f2449a3830b769d7156c6e6138a0478267bd6b
kenli6 <kenli6@remove.gmail.remove.com>
2014-01-27 18:55:15 -0800

    Lab2b Completed & My Own Version of Lab2b

---
+++ Kenneth.Li/Lab2b - My Version/Lab2b/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2b
+{
+    public partial class Form1 : Form
+    {
+        //
+
+        private Person instructor;
+        private Person mickey;
+        private Person ta;
+        private Person eva;
+        private Person askEvaForHelp = new Person();
+        private Person askInstructorForHelp = new Person();
+        private Person askMickeyForHelp = new Person();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            instructor = new Person { FirstName = "Michael", LastName = "Phoenix" };
+            mickey = instructor;
+            eva = new Person { FirstName = "Eva", LastName = "Carlstrom" };
+            ta = eva;
+
+            // Update the UI (the User Interface) with the current names.
+
+            RedisplayNames();
+        }
+
+        private void fixEvaNameButton_Click(object sender, EventArgs e)
+        {
+            // 1) Fix Eva's name
+            eva.FirstName = "Eva-Lise";
+
+            RedisplayNames();
+        }
+
+        private void fixMickeyNameButton_Click(object sender, EventArgs e)
+        {
+            // 2) Fix Mickey's name
+            mickey.FirstName = "Mickey";
+            mickey.LastName = "Phoenix"; 
+
+            RedisplayNames();
+        }
+
+        private void askForHelpButton_Click(object sender, EventArgs e)
+        {
+            // 3) Ask first the TA, and then the instructor, for help
+            askEvaForHelp.FirstName = "The Very Helpful " + ta.FirstName;
+            askEvaForHelp.LastName = eva.LastName;
+
+            //ask Mickey for help
+            askMickeyForHelp.FirstName = "The Also Helpful " + mickey.FirstName;
+            askMickeyForHelp.LastName = mickey.LastName;
+
+            //person who is the instructor right now
+            askInstructorForHelp.FirstName = "The Also Helpful " + instructor.FirstName;
+            askInstructorForHelp.LastName = instructor.LastName;
+
+
+            updateAskForHelpButtonLabels();
+        }
+
+        private void giveMickeyMartianMeaslesButton_Click(object sender, EventArgs e)
+        {
+            // 4) Mickey gets the Martian Measles, and Eva takes over as teacher for the class.
+            mickeyName.Text = "Your instructor is now " + eva.FirstName + " " + eva.LastName;
+            instructor = ta;
+        }
+
+        private void mickeyShameNameChangeButton_Click(object sender, EventArgs e)
+        {
+            // 5) In shame, Mickey changes his name to "Ex-Instructor Ashes"
+            mickey.FirstName = "Ex-Instructor";
+            mickey.LastName = "Ashes";
+            if (instructor == ta)
+            {
+                mickeyName.Text = "Your old instructor's name changed to " + mickey.FirstName + " " + mickey.LastName;
+            }
+            else
+            {
+                RedisplayNames();
+            }
+        }
+
+        private void RedisplayNames()
+        {
+            evaName.Text = eva.FirstName + " " + eva.LastName;
+            taName.Text = ta.FirstName + " " + ta.LastName;
+            mickeyName.Text = mickey.FirstName + " " + mickey.LastName;
+            instructorName.Text = instructor.FirstName + " " + instructor.LastName;
+        }
+
+        private void updateAskForHelpButtonLabels()
+        {
+            evaName.Text = askEvaForHelp.FirstName + " " + askEvaForHelp.LastName;
+            taName.Text = askEvaForHelp.FirstName + " " + askEvaForHelp.LastName;
+            instructorName.Text = askInstructorForHelp.FirstName + " " + askInstructorForHelp.LastName;
+            mickeyName.Text = askMickeyForHelp.FirstName + " " + askMickeyForHelp.LastName;
+            if (instructor == ta)
+            {
+                mickeyName.Text = "Please refer questions to " + eva.FirstName + " " + eva.LastName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
--- Kenneth.Li/Lab2b/Lab2b/Form1.cs
+++ Kenneth.Li/Lab2b/Lab2b/Form1.cs
@@ -39,16 +39,11 @@ namespace Lab2b
             eva.FirstName = "Eva-Lise";
 
             // Questions:
-            //  * What are eva.FirstName and eva.LastName? (Pre-compile) eva.FirstName is Eva-Lise, eva.LastName is Carlstrom
-            //  * What are ta.FirstName and ta.LastName? (Pre-compile) ta.FirstName is Eva-Lise, ta.LastName is Carlstrom
-            /* What are mickey.FirstName and mickey.LastName? 
-             * (My prediction) 
-             * mickey.FirstName is Michael, mickey.LastName is Phoenix, if the fixMickeyNameButton was clicked on mickey.FirstName will be Mickey, 
-             * if the mickeyShameNameChangeButton was clicked mickey.FirstName will be Ex-Instructor and mickey.LastName will be Ashes
-             */
-            //  * What are instructor.FirstName and instructor.LastName? (Pre-compile) instructor.FirstName is Michael, instructor.LastName is Phoenix
-
-            RedisplayNames();            
+            //  * What are eva.FirstName and eva.LastName? (My prediction) eva.FirstName is Eva-Lise, eva.LastName is Carlstrom
+            //  * What are ta.FirstName and ta.LastName? (My prediction) ta.FirstName is Eva-Lise, ta.LastName is Carlstrom
+            /* What are mickey.FirstName and mickey.LastName? (My prediction)mickey.FirstName is Michael, mickey.LastName is Phoenix, if the fixMickeyNameButton was clicked on mickey.FirstName will be Mickey, if the mickeyShameNameChangeButton was clicked mickey.FirstName will be Ex-Instructor and mickey.LastName will be Ashe
+             * What are instructor.FirstName and instructor.LastName? (My prediction) instructor.FirstName is Michael, instructor.LastName is Phoenix*/
+            RedisplayNames();
         }
 
         private void fixMickeyNameButton_Click(object sender, EventArgs e)
@@ -58,10 +53,11 @@ namespace Lab2b
 
             // Questions:
             // Same questions as before.
-            /*(My predictions) 
-             * mickey.FirstName will be Mickey and mickey.LastName will be Phoenix,
-             * or mickey.LastName will be Ashes if the mickeyShameNameChangeButton was clicked on prior to clicking fixMickeyNameButton
-             * instructor.FirstName will be Mickey and instructor.LastName will be Phoenix, or instructor.LastName can be Ashes if mickeyShameNameChangeButton was clicked at any point
+            /*(My predictions)
+             * What are eva.FirstName and eva.LastName? (My prediction) eva.FirstName is Eva-Lise, eva.LastName is Carlstrom
+             * What are ta.FirstName and ta.LastName? (My prediction) ta.FirstName is Eva-Lise, ta.LastName is Carlstrom
+             * What are mickey.FirstName and mickey.LastName? (My prediction) mickey.FirstName will be Mickey and mickey.LastName will be Phoenix,or mickey.LastName will be Ashes if the mickeyShameNameChangeButton was clicked on prior to clicking fixMickeyNameButton
+             * What are instructor.FirstName and instructor.LastName? (My prediction) instructor.FirstName will be Mickey and instructor.LastName will be Phoenix, or instructor.LastName can be Ashes if mickeyShameNameChangeButton was clicked at any point
              * If the fixEvaNameButton was not clicked, eva.FirstName will be Eva and eva.LastName will be Carlstrom
              * If the fixEvaNameButton was clicked, eva.FirstName will be Eva-Lise and eva.LastName will be Carlstrom
             */
@@ -78,10 +74,13 @@ namespace Lab2b
 
             /* Same questions... 
              * (My predictions)  
-             * If the fixEvaNameButton and fixMickeyNameButton were both clicked on, it will display The Very Helpful Eva-Lise and then The Also Helpful Mickey
-             * if the fixEvaNameButton and fixMickeyNameButton were both NOT clicked on, it will display The Very Helpful Eva and then The Also Helpful Michael
-             * if the fixEvaNameButton was clicked on and fixMickeyNameButton was NOT clicked on, it will display The Very Helpful Eva-Lise and then Also Helpful Michael
-             * if the fixEvaNameButton was NOT clicked and fixMickeyNameButton was clicked, it will display The Very Helpful Eva and then The Also Helpful Mickey
+             * What are eva.FirstName and eva.LastName? (My prediction) If the fixEvaNameButton, it will display The Very Helpful Eva-Lise Carlstrom
+             * What are ta.FirstName and ta.LastName? (My prediction) If the fixEvaNameButton was clicked on, it will display The Very Helpful Eva-Lise Carlstrom
+             * What are mickey.FirstName and mickey.LastName? (My prediction) if the fixEvaNameButton and fixMickeyNameButton were both NOT clicked on, it will display The Also Helpful Michael Phoenix. If the fixEvaNameButton was clicked on and fixMickeyNameButton was NOT clicked on, then Also Helpful Michael Phoenix
+             * What are instructor.FirstName and instructor.LastName? (My predictions)
+             * if the fixEvaNameButton and fixMickeyNameButton were both NOT clicked on, it will display The Very Helpful Eva Carlstrom
+             * if the fixEvaNameButton was clicked on and fixMickeyNameButton was NOT clicked on, it will display The Very Helpful Eva-Lise Carlstrom
+             * if the fixEvaNameButton was NOT clicked and fixMickeyNameButton was clicked, it will display The Very Helpful Eva Carlstrom and then The Also Helpful Mickey Phoenix
              */
 
             RedisplayNames();
@@ -94,10 +93,12 @@ namespace Lab2b
 
             /* Same questions...
              * (My predictions) 
-             * if the fixEvaNameButton was clicked instructor.FirstName will be Eva-Lise and instructor.LastName will be Carlstrom
-             * if the fixEvaNamebutton was not clicked instructor.FirstName will be Eva and instructor.LastName will be Carlstrom
+             * What are eva.FirstName and eva.LastName? (My prediction) eva.FirstName is Eva-Lise, eva.LastName is Carlstrom if the fixEvaNameButton was pressed eva.FirstName is Eva-Lise and eva.LastName is Carlstrom
+             * What are ta.FirstName and ta.LastName? (My prediction) ta.FirstName is Eva-Lise, ta.LastName is Carlstrom
+             * What are mickey.FirstName and mickey.LastName? (My prediction)
              * mickey.FirstName will be Michael if theFixMickeyNameButton was not clicked, mickey.FirstName will be Mickey if theFixMickeyNameButton was clicked, mickey.FirstName will be Ex-Instructor if the mickeyShameNameChangeButton was clicked
              * mickey.LastName will be Phoenix or it will be Ashes if mickeyShameNameChangeButton was clicked
+             * What are instructor.FirstName and instructor.LastName? (My predictions) if the fixEvaNamebutton was not clicked instructor.FirstName will be Eva and instructor.LastName will be Carlstrom
             */
             RedisplayNames();
         }
@@ -109,14 +110,12 @@ namespace Lab2b
             mickey.LastName = "Ashes";
 
             /* Same questions... 
-             * (My predictions) 
-             * mickey.FirstName will be Ex-Instructor, mickey.LastName will be Ashes.
-             * if the fixMickeyNameButton was clicked after clicking mickeyShameNameChangeButton mickey.FirstName will change to Mickey and mickey.LastName will stay as Ashes
-             * eva.LastName will be Carlstrom
-             * eva.FirstName will be Eva-Lise if the fixEvaNameButton was clicked or Eva if the fixEvaNameButton was NOT clicked
+             * (My predictions)
+             * What are eva.FirstName and eva.LastName? (My prediction) eva.FirstName is Eva-Lise, eva.LastName is Carlstrom if the fixEvaNameButton was pressed eva.FirstName is Eva-Lise and eva.LastName is Carlstrom 
+             * What are ta.FirstName and ta.LastName? (My predicdtion) ta.FirstName is Eva-Lise, ta.LastName is Carlstrom if the fixEvaNameButton was pressed ta.FirstName is Eva-Lise and ta.LastName is Carlstrom
+             * mickey.FirstName will be Ex-Instructor, mickey.LastName will be Ashes. If the fixMickeyNameButton was clicked after clicking mickeyShameNameChangeButton mickey.FirstName will change to Mickey and mickey.LastName will stay as Ashes
+             * What are instructor.FirstName and instructor.LastName? (My predictions)  instructor.FirstName is Eva-Lise, instructor.LastName is Carlstrom if the fixEvaNameButton was pressed instructor.FirstName is Eva-Lise and instructor.LastName is Carlstrom
              */
-
-            RedisplayNames();
         }
 
         private void RedisplayNames()
@@ -126,5 +125,17 @@ namespace Lab2b
             mickeyName.Text = mickey.FirstName + " " + mickey.LastName;
             instructorName.Text = instructor.FirstName + " " + instructor.LastName;
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
+    /*
+     * My predictions were fairly close, but not perfect. I did not take into account of what would happen if askForHelpButton was clicked on and how that would effect the RedisplayNames() method.
+     * If the askForHelpButton is hit multiple times, the labels will keep printing The Very Helpful The Also Helpful with Eva or Eva-Lise followed by Carlstrom or The Also Helpful The Very Helpful followed by Michael, Mickey or Ex-Instructor followed by either Phoenix or Ashes.
+     * The program will always display mickey.LastName as Ashes if the mickeyShameNameButton is clicked and there is no way to revert mickey.LastName to Pheonix.
+     * The fixMickeyNameButton needs to include a mickey.LastName = Phoenix, otherwise if the mickeyShameNameChangeButton is clicked first, hitting the fixMickeyNameButton will result in Mickey Ashe (unless that was the actual intent of the developer).
+     */
 }
