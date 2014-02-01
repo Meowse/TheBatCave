51d9e39f786c63377e7d31784010388d9d527ed3
kenli6 <kenli6@remove.gmail.remove.com>
2014-02-01 03:06:07 -0800

    Homework 3 - Complete

--- Kenneth.Li/Homework/Session 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
+++ Kenneth.Li/Homework/Session 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -1,4 +1,7 @@
-﻿namespace ExploringCSharp
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace ExploringCSharp
 {
     public class DoingMath
     {
@@ -10,13 +13,16 @@
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            return 0;
+
+            return Math.Max(number1, number2);
         }
 
         public int GetBiggestMagnitude(int number1, int number2)
         {
             // Try googling "C# absolute value of a number"
-            return 0;
+            var absoluteOfNumber1 = Math.Abs(number1);
+            var absoluteOfNumber2 = Math.Abs(number2);
+            return absoluteOfNumber1 > absoluteOfNumber2 ? number1 : number2;
         }
 
         public int MultiplyByTheNextLargerPowerOfTen(int number)
@@ -24,7 +30,19 @@
             // Try googling "C# exponents and logarithms".  Or just "exponents and logarithms",
             // if college math was too long ago for you (I had to look it up the last time I needed
             // to do this, so don't feel bad if you do, too).
-            return 0;
+            //double logOfNumber = Math.Log(number);
+            var lengthOfNumber = (number.ToString().Length);
+            var multiplier = Math.Pow(10, lengthOfNumber);
+            if (number == 1)
+            {
+                multiplier = 1;
+            }
+            var result = number * multiplier;
+            if (multiplier == Math.Pow(10, lengthOfNumber))
+            {
+                result = Math.Pow(number, 2);
+            }
+            return (int) result;
         }
     }
 }
