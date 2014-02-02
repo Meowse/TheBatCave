6ed11c2db0b70b0ed8cf88bd4db39a6d69043a60
MatthewSWest <MatWes@Microsoft.com>
2014-02-01 16:29:11 -0800

    Homework 3 Final

--- Matt.West/Home Work/Session 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
+++ Matt.West/Home Work/Session 3/ExploringCSharp/ExploringCSharp/DoingMath.cs
@@ -1,4 +1,6 @@
-﻿namespace ExploringCSharp
+﻿using System;
+
+namespace ExploringCSharp
 {
     public class DoingMath
     {
@@ -10,13 +12,14 @@
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            return 0;
+           return Math.Max(number1, number2);
         }
 
         public int GetBiggestMagnitude(int number1, int number2)
         {
             // Try googling "C# absolute value of a number"
-            return 0;
+            var bigger = Math.Max(Math.Abs(number1), Math.Abs(number2));
+            return bigger == Math.Abs(number1) ? number1 : number2;
         }
 
         public int MultiplyByTheNextLargerPowerOfTen(int number)
@@ -24,7 +27,35 @@
             // Try googling "C# exponents and logarithms".  Or just "exponents and logarithms",
             // if college math was too long ago for you (I had to look it up the last time I needed
             // to do this, so don't feel bad if you do, too).
-            return 0;
+
+
+            var scale = (Math.Pow(10, (int)Math.Log10(number)))*10;
+            return number != 1 ? ((scale/10) == number ? number*number : (int) (number*scale)) : number;
+
+
+//
+//
+//                   [Test]
+//        public void ShouldMultiplyNumbersByTheirPowerOfTen()
+//        {
+//            Assert.That(_doingMath.MultiplyByTheNextLargerPowerOfTen(9), Is.EqualTo(90));
+//            Assert.That(_doingMath.MultiplyByTheNextLargerPowerOfTen(88), Is.EqualTo(8800));
+//            Assert.That(_doingMath.MultiplyByTheNextLargerPowerOfTen(101), Is.EqualTo(101000));
+//        }
+//
+//        [Test]
+//        public void ShouldMultiplyPowersOfTenByThemselves()
+//        {
+//            Assert.That(_doingMath.MultiplyByTheNextLargerPowerOfTen(10), Is.EqualTo(100));
+//            Assert.That(_doingMath.MultiplyByTheNextLargerPowerOfTen(100), Is.EqualTo(10000));
+//            Assert.That(_doingMath.MultiplyByTheNextLargerPowerOfTen(1000), Is.EqualTo(1000000));
+//        }
+//
+//        [Test]
+//        public void OneShouldReturnOneSinceOneIsTheNextLargerPowerOfTen()
+//        {
+//            Assert.That(_doingMath.MultiplyByTheNextLargerPowerOfTen(1), Is.EqualTo(1));
+//        }
         }
     }
 }
