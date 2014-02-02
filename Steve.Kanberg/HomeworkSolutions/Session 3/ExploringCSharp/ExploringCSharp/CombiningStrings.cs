c55276f5df7d727fb7999a2b81b7a4fa66c370e6
vstevka <vstevka@hotmail.com>
2014-02-02 13:13:25 -0800

    HW3 Solutions CS E.C. Check-In 5
    CombiningStrings.cs Extra Credit

--- Steve.Kanberg/HomeworkSolutions/Session 3/ExploringCSharp/ExploringCSharp/CombiningStrings.cs
+++ Steve.Kanberg/HomeworkSolutions/Session 3/ExploringCSharp/ExploringCSharp/CombiningStrings.cs
@@ -18,6 +18,9 @@ namespace ExploringCSharp
         public string GreetsByCombiningStringsWithStringBuilder(string name)
         {
             StringBuilder builder = new StringBuilder(100);
+
+            builder.Insert(0, 4);
+
             // Try typing "builder." and seeing what auto-complete options ReSharper gives you.
             return builder.ToString();
         }
