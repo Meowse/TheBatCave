63e459a75372e262ec15d34b0c209027cd52a931
eyshih <ed.shih@gmail.com>
2014-02-04 10:37:35 -0800

    Revert "update2 check-in HW3"
    This reverts commit 88212f2290aa08f5a98b67cc92cd8f35cca59a95.

--- Ed.Shih/ed.shih_homework03/ExploringCSharp/CombiningStrings.cs
+++ Ed.Shih/ed.shih_homework03/ExploringCSharp/CombiningStrings.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.Text;
+﻿using System.Text;
 
 namespace ExploringCSharp
 {
@@ -14,17 +12,13 @@ namespace ExploringCSharp
         public string GreetsByCombiningStringsWithFormats(string name)
         {
             // try googling "string formatting in C#"
-            // tried to create a Name object and then do String.Format("Hello {name}" but that broke the test
-            string greeting = String.Format("Hello, {0}", "Mickey");
-            return greeting;
-            // this can get the correct test result but I thought the way I tried above would be the more common way to use String.Format
+            return null;
         }
 
         public string GreetsByCombiningStringsWithStringBuilder(string name)
         {
             StringBuilder builder = new StringBuilder(100);
             // Try typing "builder." and seeing what auto-complete options ReSharper gives you.
-            // Typing "builder" brought up GreetsByCombiningStringsWithStringBuilder, StringBuilder, and UniBuilder.
             return builder.ToString();
         }
     }
