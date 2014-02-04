b91286311618f6269310da793d33b580c60f1620
eyshih <ed.shih@gmail.com>
2014-02-04 11:33:28 -0800

    update4 check-in HW4
    two methods for DoingMath pass; still working on the final one

--- Ed.Shih/ed.shih_homework03/ExploringCSharp/DoingMath.cs
+++ Ed.Shih/ed.shih_homework03/ExploringCSharp/DoingMath.cs
@@ -1,4 +1,7 @@
-﻿namespace ExploringCSharp
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace ExploringCSharp
 {
     public class DoingMath
     {
@@ -10,13 +13,14 @@
             // Notice also that ReSharper is *too* helpful, here, and keeps trying to turn "Math." into 
             // "DoingMath.".  Play with it until you figure out how to type "Math." without ReSharper
             // changing it into "DoingMath." on you.
-            return 0;
+            return Math.Max(number1, number2);
+            // I managed to get it to type "Math" by typing out the full word and hitting Enter without choosing one of the suggestions.
         }
 
         public int GetBiggestMagnitude(int number1, int number2)
         {
             // Try googling "C# absolute value of a number"
-            return 0;
+            return Math.Abs(number1) > Math.Abs(number2) ? number1 : number2;
         }
 
         public int MultiplyByTheNextLargerPowerOfTen(int number)
@@ -24,7 +28,8 @@
             // Try googling "C# exponents and logarithms".  Or just "exponents and logarithms",
             // if college math was too long ago for you (I had to look it up the last time I needed
             // to do this, so don't feel bad if you do, too).
-            return 0;
+            return (int) (number*(Math.Log10(number)+1));
         }
+
     }
 }
