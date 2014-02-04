015fa9eca5dba3fdd56585207a0e6e09cd2e5211
bfeigert <bfeigert@gmail.com>
2014-02-04 11:39:53 -0700

    Exploring CSharp boolean tests pass
    Made the boolean tests pass. No attempt yet on extra credit.

--- Ben.Feigert/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
+++ Ben.Feigert/ExploringCSharp/ExploringCSharp/BooleanLogic.cs
@@ -4,52 +4,42 @@
     {
         public bool NegatesItsInput(bool input)
         {
-            if (input == true)
+            if (input)
             {
                 return false;
             }
-            else
-            {
-                return true;
-            }
+            return true;
         }
 
         public bool NegatesItsInputSingleLine(bool input)
         {
-            // Use resharper on the above to reduce it to a single line.
-            return false;
+            return !input;
+
+            // ### Used resharper on the above to reduce it to a single line.
         }
 
         public bool TrueIfBothInputsAreTrue(bool input1, bool input2)
         {
-            if (input1 == true)
+            if (input1)
             {
-                if (input2 == true)
+                if (input2)
                 {
                     return true;
                 }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
-            else
+            if (input1)
             {
-                if (input1 == true)
-                {
-                    return false;
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
+            return false;
         }
 
         public bool TrueIfBothInputsAreTrueSingleLine(bool input1, bool input2)
         {
-            // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 && input2;
+
+            // ### Used resharper on the above to reduce it to a single line.
         }
 
         public bool TrueIfEitherInputIsTrue(bool input1, bool input2)
@@ -61,28 +51,20 @@
                 {
                     return true;
                 }
-                else
-                {
-                    return true;
-                }
+                return true;
             }
-            else
+            if (input2 == true)
             {
-                if (input2 == true)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return true;
             }
+            return false;
         }
 
         public bool TrueIfEitherInputIsTrueSingleLine(bool input1, bool input2)
         {
-            // Use resharper on the above to reduce it to a single line.
-            return false;
+            return input1 == true ? input2 == true || true : input2 == true;
+
+            //### Used resharper on the above to reduce it to a single line.
         }
 
         public bool MustPayExtraSurchargeToRentACar(string gender, int age)
