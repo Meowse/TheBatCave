3763ca36e04d937fc3440885e079a6b0ce8792f6
bfeigert <bfeigert@gmail.com>
2014-02-04 13:18:56 -0700

    Exploring CSharp Conditionals: all tests pass
    all done with the regular-credit assignments. will attempt extra credit.

--- Ben.Feigert/ExploringCSharp/ExploringCSharp/ExploringConditionals.cs
+++ Ben.Feigert/ExploringCSharp/ExploringCSharp/ExploringConditionals.cs
@@ -40,7 +40,12 @@
             // and have something more complicated to do with normal input.  You just check for
             // the invalid input, and return if you find it.  The rest of the method can be as 
             // complicated as you want, and it doesn't need to be "nested" inside an "else" clause.
-            return "";
+            {
+                if (stringToDouble == null)
+                    return null;
+
+                return stringToDouble + stringToDouble;
+            }
         }
 
         public string ComplexConditionUsingElseIf(int value)
@@ -69,15 +74,45 @@
 
         public string ComplexConditionUsingReturnsAndIfs(int value)
         {
-            // Use Resharper on the above to get rid of all of the "else" clauses
-            return "";
+            // ### Used Resharper on the above to get rid of all of the "else" clauses
+
+            if (value == 0)
+            {
+                return "Free!";
+            }
+            if (value == 1)
+            {
+                return "Cheap as dirt!";
+            }
+            if (value == 2)
+            {
+                return "Twice as expensive as dirt...";
+            }
+            if (value == 3)
+            {
+                return "TANSTAAFL: There ain't no such thing as a free lunch.";
+            }
+            return "Too rich for my blood!";
         }
 
         public string ComplexConditionUsingSwitch(int value)
         {
-            // use Resharper on the first ComplexCondition to convert the entire thing
+            //####  used Resharper on the first ComplexCondition to convert the entire thing
             // to a switch statement.
-            return "";
+
+            switch (value)
+            {
+                case 0:
+                    return "Free!";
+                case 1:
+                    return "Cheap as dirt!";
+                case 2:
+                    return "Twice as expensive as dirt...";
+                case 3:
+                    return "TANSTAAFL: There ain't no such thing as a free lunch.";
+                default:
+                    return "Too rich for my blood!";
+            }
         }
     }
 }
