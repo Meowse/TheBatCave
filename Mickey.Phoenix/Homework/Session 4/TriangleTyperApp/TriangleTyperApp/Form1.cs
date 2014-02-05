b30c65798bf62c312f267248211ad3118f8c4e84
Meowse <meowse@remove.gmail.remove.com>
2014-02-04 20:32:11 -0800

    Homework 4 pre-refactoring

---
+++ Mickey.Phoenix/Homework/Session 4/TriangleTyperApp/TriangleTyperApp/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TriangleTyperApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void typeItButton_Click(object sender, EventArgs e)
+        {
+            TriangleTypeCalculator calculator = new TriangleTypeCalculator();
+            string sideA = sideAField.Text;
+            string sideB = sideBField.Text;
+            string sideC = sideCField.Text;
+            string triangleType = calculator.GetTriangleType(sideA, sideB, sideC);
+            triangleTypeDisplay.Text = triangleType;
+        }
+    }
+}
