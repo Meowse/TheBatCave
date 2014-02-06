39e7e29c70f7bd58300fa0317b2b5aeafc1c0609
kenli6 <kenli6@remove.gmail.remove.com>
2014-02-05 23:16:33 -0800

    Homework 4 - Version 1
    Not all of the tests have been implemented. Still have a few other items
    to work through.

--- Kenneth.Li/Homework/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
+++ Kenneth.Li/Homework/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
@@ -12,6 +12,19 @@ namespace TriangleTypeCalculatorTest
         public void TestEquilateral()
         {
             Assert.That(_calculator.GetTriangleType("3", "3", "3"), Is.EqualTo("Equilateral"));
+            Assert.That(_calculator.GetTriangleType("3", "3", "3.0"), Is.EqualTo("Equilateral"));
+        }
+        [Test]
+        public void TestIsosceles()
+        {
+            Assert.That(_calculator.GetTriangleType("5", "5", "2"), Is.EqualTo("Isosceles"));
+            Assert.That(_calculator.GetTriangleType("2,147,483,647", "2,147,483,647", "1"), Is.EqualTo("Isosceles"));
+        }
+
+        [Test]
+        public void TestScalene()
+        {
+            Assert.That(_calculator.GetTriangleType("3", "6", "7"), Is.EqualTo("Scalene"));
         }
     }
 }
