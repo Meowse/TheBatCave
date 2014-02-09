17453eca2f2ca075900bc72ce29a20b8d0e986b7
ShaunaB <shauna.bennett@paccar.com>
2014-02-08 17:15:47 -0800

    Homework 4 with Tests

--- Shauna.Bennett/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
+++ Shauna.Bennett/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -1,7 +1,14 @@
-﻿namespace TriangleTyperApp
+﻿using System;
+
+namespace TriangleTyperApp
 {
     public class TriangleTypeCalculator
     {
+        private decimal _nbrA;
+        private decimal _nbrB;
+        private decimal _nbrC;
+        
+        
         public string GetTriangleType(string sideA, string sideB, string sideC)
         {
             return "Equilateral";
