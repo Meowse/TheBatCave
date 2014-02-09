46b822715838cb86edf5e1dae955ccbcb60cb1e5
kalismithson <kalismithson@gmail.com>
2014-02-08 21:33:09 -0800

    Calculator Tests - Build Project Error...?

--- Kali.Smithson/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
+++ Kali.Smithson/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
 using TriangleTyperApp;
 
 namespace TriangleTypeCalculatorTest
@@ -12,6 +13,19 @@ namespace TriangleTypeCalculatorTest
         public void TestEquilateral()
         {
             Assert.That(_calculator.GetTriangleType("3", "3", "3"), Is.EqualTo("Equilateral"));
+            Assert.That(_calculator.GetTriangleType("5", "5", "5"), Is.EqualTo("Equilateral"));
         }
-    }
-}
+
+        [Test]
+        public void TestIsoceles()
+        {
+            Assert.That(_calculator.GetTriangleType("8", "7", "8"), Is.EqualTo("Isoceles"));
+            Assert.That(_calculator.GetTriangleType("10", "10", "82"), Is.EqualTo("Isoceles"));
+        }
+
+        [Test]
+        public void TestScalene()
+        {
+            Assert.That(_calculator.GetTriangleType("8", "9", "10"), Is.EqualTo("Scalene"));
+            Assert.That(_calculator.GetTriangleType("800", "90", "201"), Is.EqualTo("Scalene"));
+        }
