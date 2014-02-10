6dcc72c1181a4cc904488a32ac4bc1874be0e406
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-02-09 17:10:54 -0800

    Handles non-numeric and negative numbers

---
+++ Mickey.Phoenix/HomeworkSolutions/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/ErrorHandlingTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TriangleTyperApp;
+
+namespace TriangleTypeCalculatorTest
+{
+    [TestFixture]
+    class ErrorHandlingTests
+    {
+        private readonly TriangleTypeCalculator _calculator = new TriangleTypeCalculator();
+
+        [Test]
+        public void NonNumbersFail()
+        {
+            Assert.That(_calculator.GetTriangleType("three", "four", "five"), Is.EqualTo("Inputs must be integers"));            
+        }
+
+        [Test]
+        public void NegativeNumbersFail()
+        {
+            Assert.That(_calculator.GetTriangleType("-3", "-4", "-5"), Is.EqualTo("A triangle cannot have sides of negative length"));
+        }
+    }
+}
--- Mickey.Phoenix/HomeworkSolutions/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
+++ Mickey.Phoenix/HomeworkSolutions/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -1,9 +1,40 @@
-﻿namespace TriangleTyperApp
+﻿using System;
+using System.Collections.Generic;
+
+namespace TriangleTyperApp
 {
     public class TriangleTypeCalculator
     {
         public string GetTriangleType(string sideA, string sideB, string sideC)
         {
+            int a;
+            if (!int.TryParse(sideA, out a))
+            {
+                return "Inputs must be integers";
+            }
+
+            int b;
+            if (!int.TryParse(sideA, out b))
+            {
+                return "Inputs must be integers";
+            }
+
+            int c;
+            if (!int.TryParse(sideA, out c))
+            {
+                return "Inputs must be integers";
+            }
+
+            return GetTriangleType(a, b, c);
+        }
+
+        private string GetTriangleType(int a, int b, int c)
+        {
+            if ((a < 0) || (b < 0) || (c < 0))
+            {
+                return "A triangle cannot have sides of negative length";
+            }
+
             return "Equilateral";
         }
     }
