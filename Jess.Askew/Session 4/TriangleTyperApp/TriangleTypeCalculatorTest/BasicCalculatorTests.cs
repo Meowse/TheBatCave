2f9d59751c15c59cd397cf5005d8f7b12a696335
slightlyaskew <jessica.e.askew@gmail.com>
2014-02-10 21:30:41 -0800

    Iso/Equal pass

--- Jess.Askew/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
+++ Jess.Askew/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
@@ -12,6 +12,14 @@ namespace TriangleTypeCalculatorTest
         public void TestEquilateral()
         {
             Assert.That(_calculator.GetTriangleType("3", "3", "3"), Is.EqualTo("Equilateral"));
+            Assert.That(_calculator.GetTriangleType("5", "5", "5"), Is.EqualTo("Equilateral"));
         }
+
+        [Test]
+        public void TestIsosceles()
+        {
+            Assert.That(_calculator.GetTriangleType("5", "5", "10"), Is.EqualTo("Isosceles"));
+        }    
+
     }
 }
--- Jess.Askew/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
+++ Jess.Askew/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -1,10 +1,27 @@
-﻿namespace TriangleTyperApp
+﻿using System.Runtime.Remoting.Messaging;
+
+namespace TriangleTyperApp
 {
     public class TriangleTypeCalculator
     {
         public string GetTriangleType(string sideA, string sideB, string sideC)
         {
-            return "Equilateral";
+            if (sideA == sideB && sideB == sideC)
+            {
+                return "Equilateral";
+            }
+
+            if (sideA == sideB || sideB == sideC || sideC == sideA)
+            {
+                return "Isosceles";
+            }
+
+        // if (sideA + sideB > sideC || sideB + sideC > sideA || sideA + sideC > sideB)
+       //  {
+       //      return "Scalene"
+      //   } 
+            return "Nope.";
         }
     }
 }
+
