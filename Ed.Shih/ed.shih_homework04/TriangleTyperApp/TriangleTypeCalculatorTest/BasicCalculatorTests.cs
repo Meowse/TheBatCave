d6857729a2522d456fcaf024d58589651a7e4dbd
eyshih <ed.shih@gmail.com>
2014-02-11 08:26:12 -0800

    HW4 update 4
    integer and range validation tests pass

--- Ed.Shih/ed.shih_homework04/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
+++ Ed.Shih/ed.shih_homework04/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
@@ -26,22 +26,22 @@ namespace TriangleTypeCalculatorTest
         [Test]
         public void TestIntRange1()
         {
-            Assert.That(_calculator.GetTriangleType("0", "0", "0"), Is.EqualTo("Sides must be greater than 0 and less than 2,000,000,000"));
+            Assert.That(_calculator.GetTriangleType("0", "0", "0"), Is.EqualTo("Sides must be greater than 0 and less than 2,000,000,001"));
         }
         [Test]
         public void TestIntRange2()
         {
-            Assert.That(_calculator.GetTriangleType("-4", "4", "20"), Is.EqualTo("Sides must be greater than 0 and less than 2,000,000,000"));
+            Assert.That(_calculator.GetTriangleType("-4", "4", "20"), Is.EqualTo("Sides must be greater than 0 and less than 2,000,000,001"));
         }
         [Test]
         public void TestIntRange3()
         {
-            Assert.That(_calculator.GetTriangleType("2000000001", "2000000001", "2000000001"), Is.EqualTo("Sides must be greater than 0 and less than 2,000,000,000"));
+            Assert.That(_calculator.GetTriangleType("2000000001", "2000000001", "2000000001"), Is.EqualTo("Sides must be greater than 0 and less than 2,000,000,001"));
         }
         [Test]
         public void TestIntRange4()
         {
-            Assert.That(_calculator.GetTriangleType("2000000001", "0", "2000000001"), Is.EqualTo("Sides must be greater than 0 and less than 2,000,000,000"));
+            Assert.That(_calculator.GetTriangleType("2000000001", "0", "2000000001"), Is.EqualTo("Sides must be greater than 0 and less than 2,000,000,001"));
         }
         [Test]
         public void TestIsTriangle1()
@@ -86,7 +86,7 @@ namespace TriangleTypeCalculatorTest
         [Test]
         public void TestIsoscoles3()
         {
-            Assert.That(_calculator.GetTriangleType("2,147,483,647", "2,147,483,647", "1"), Is.EqualTo("Isoscoles"));
+            Assert.That(_calculator.GetTriangleType("2000000000", "2000000000", "1"), Is.EqualTo("Isoscoles"));
         }
         [Test]
         public void TestScalene1()
@@ -101,7 +101,7 @@ namespace TriangleTypeCalculatorTest
         [Test]
         public void TestScalene3()
         {
-            Assert.That(_calculator.GetTriangleType("2,147,483,647", "2,147,483,646", "2"), Is.EqualTo("Scalene"));
+            Assert.That(_calculator.GetTriangleType("2000000000", "1999999999", "2"), Is.EqualTo("Scalene"));
         }
     }
 }
--- Ed.Shih/ed.shih_homework04/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
+++ Ed.Shih/ed.shih_homework04/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -10,29 +10,54 @@ namespace TriangleTyperApp
             int A;
             if (!int.TryParse(sideA, out A))
             {
-                return "Please enter an Integer for Side A";
+                return "Make sure all sides are Integers";
             }
             int B;
             if (!int.TryParse(sideA, out B))
             {
-                return "Please enter an Integer for Side B";
+                return "Make sure all sides are Integers";
             }
             int C;
             if (!int.TryParse(sideA, out C))
             {
-                return "Please enter an Integer for Side C";
+                return "Make sure all sides are Integers";
             }
-            // check to see if 0 < int < MaxInt            
+            // check to see if 0 < int < MaxInt
+            // changing design to limit range from 0 to 2000000000
+            if (A < 1)
+            {
+                return "Sides must be greater than 0 and less than 2,000,000,001";
+            }
+            if (A > 2000000000)
+            {
+                return "Sides must be greater than 0 and less than 2,000,000,001";
+            }
+            if (B < 1)
+            {
+                return "Sides must be greater than 0 and less than 2,000,000,001";
+            }
+            if (B > 2000000000)
+            {
+                return "Sides must be greater than 0 and less than 2,000,000,001";
+            }
+            if (C < 1)
+            {
+                return "Sides must be greater than 0 and less than 2,000,000,001";
+            }
+            if (C > 2000000000)
+            {
+                return "Sides must be greater than 0 and less than 2,000,000,001";
+            }
             // check to see if sideA + sideB <= sideC
             // check to see if sideB + sideC <= sideA
             // check to see if sideA + sideC <= sideB
             // check to see if A = B && B = C
-            if (A == B && B == C)
+            if (sideA == sideB && sideB == sideC)
             {
                 return "Equilateral";
             }
             // check to see if A = B || B = C || A = C
-            if (A == B || B == C || A == C)
+            if (sideA == sideB || sideB == sideC || sideA == sideC)
             {
                 return "Isoscoles";
             }
