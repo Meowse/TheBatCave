80a7eb8f3d15a1fea215ac9446f084af0370b799
kalismithson <kalismithson@gmail.com>
2014-02-10 18:34:10 -0800

    Session 4 Homework - Complete

--- Kali.Smithson/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
+++ Kali.Smithson/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -1,10 +1,26 @@
-﻿namespace TriangleTyperApp
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace TriangleTyperApp
 {
     public class TriangleTypeCalculator
     {
         public string GetTriangleType(string sideA, string sideB, string sideC)
         {
-            return "Equilateral";
+            if ((sideA == sideB) && (sideB == sideC))
+
+                return "Equilateral";
+
+            if ((sideA != sideB) && (sideB != sideC) && (sideC != sideA))
+
+                return "Scalene";
+
+            return "Isoceles";
+
         }
+
     }
+
 }
