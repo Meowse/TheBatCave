d8e5fbe0e96e718a59663582288ff583ffca5944
eyshih <ed.shih@gmail.com>
2014-02-11 00:01:39 -0800

    HW update 2
    added simple logic checks to compare sides as strings

--- Ed.Shih/ed.shih_homework04/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
+++ Ed.Shih/ed.shih_homework04/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -4,7 +4,22 @@
     {
         public string GetTriangleType(string sideA, string sideB, string sideC)
         {
-            return "Equilateral";
+            // try to convert string to int
+            // check to see if 0 < int < MaxInt
+            // check to see if sideA + sideB <= sideC
+            // check to see if sideB + sideC <= sideA
+            // check to see if sideA + sideC <= sideB
+            // check to see if A = B && B = C
+            if (sideA == sideB && sideB == sideC)
+            {
+                return "Equilateral";
+            }
+            // check to see if A = B || B = C || A = C
+            if (sideA == sideB || sideB == sideC || sideA == sideC)
+            {
+                return "Isoscoles";
+            }
+            return "Scalene";
         }
     }
 }
