5b2a6796aa73636457cb2a6503c95133bc2cbdd5
bfeigert <bfeigert@gmail.com>
2014-02-11 14:36:08 -0700

    Triangle Typer V2
    Now with deleted non-functioning calculator app. Have copied over MP's
    calculator app, after much misery with my own.

---
+++ Ben.Feigert/TriangleTyperAppBF/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace TriangleTyperApp
+{
+    public class TriangleTypeCalculator
+    {
+        public string GetTriangleType(string sideA, string sideB, string sideC)
+        {
+            int a;
+            if (!int.TryParse(sideA, out a))
+            {
+                return "Inputs must be integers";
+            }
+
+            int b;
+            if (!int.TryParse(sideA, out b))
+            {
+                return "Inputs must be integers";
+            }
+
+            int c;
+            if (!int.TryParse(sideA, out c))
+            {
+                return "Inputs must be integers";
+            }
+
+            return GetTriangleType(a, b, c);
+        }
+
+        private string GetTriangleType(int a, int b, int c)
+        {
+            if ((a <= 0) || (b <= 0) || (c <= 0))
+            {
+                return "All sides of a triangle must have positive length";
+            }
+
+            return "Equilateral";
+        }
+    }
+}
