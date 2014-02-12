4876484217e8f063bb7e1f389eb07311c2cab9b8
Meowse <meowse@remove.gmail.remove.com>
2014-02-11 18:15:09 -0800

    Official solution baseline

---
+++ Mickey.Phoenix/HomeworkSolutions/Session 4/TriangleTyperApp - Solution/TriangleTypeCalculatorTest/EvilNonTriangleTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TriangleTyperApp;
+
+namespace TriangleTypeCalculatorTest
+{
+    [TestFixture]
+    public class EvilNonTriangleTests
+    {
+        private readonly TriangleTypeCalculator _calculator = new TriangleTypeCalculator();
+
+        [Test]
+        public void VeryLongSidesMustMakeTriangle()
+        {
+            Assert.That(IsError(_calculator.GetTriangleType("1", int.MaxValue.ToString(), "1")), Is.True);
+            Assert.That(IsError(_calculator.GetTriangleType(int.MaxValue.ToString(), "1", "1")), Is.True);
+            Assert.That(IsError(_calculator.GetTriangleType("1", "1", int.MaxValue.ToString())), Is.True);
+        }
+
+        private bool IsError(string result)
+        {
+            return ((result != "Equilateral") && (result != "Isosceles") && (result != "Scalene"));
+        }
+    }
+}
