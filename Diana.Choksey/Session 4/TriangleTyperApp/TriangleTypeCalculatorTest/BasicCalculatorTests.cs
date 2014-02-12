4f50516c7056f743c733c63841c537a0e75a9087
Diana Choksey <d_choksey@hotmail.com>
2014-02-11 16:09:55 -0800

    Homework 4
    Triangle Typer App with checks that the inputs are integers and the
    triangles are scalene, isoceles and equilateral.
    
    Also added checks / code to check that integer input is actually a
    triangle.
    
    Want to add other parameters to the TriangleTypeCalculator and the test,
    but have questions before I move forward.

---
+++ Diana.Choksey/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using TriangleTyperApp;
+
+namespace TriangleTypeCalculatorTest
+{
+    [TestFixture]
+    public class BasicCalculatorTests
+    {
+        private readonly TriangleTypeCalculator _calculator = new TriangleTypeCalculator();
+
+        [Test]
+        public void TestInteger1()
+        {
+            Assert.That(_calculator.GetTriangleType("MEOUSE", "&", "BAA@#@$"), Is.EqualTo("Make sure all sides are Integers"));
+        }
+
+        [Test]
+        public void TestForTriangl()
+        {
+            Assert.That(_calculator.GetTriangleType("1", "2", "3"), Is.EqualTo("Sorry Dave these sides do not a triangle make"));
+        }
+
+
+        [Test]
+        public void TestEquilateral()
+        {
+            Assert.That(_calculator.GetTriangleType("3", "3", "3"), Is.EqualTo("Equilateral"));
+            Assert.That(_calculator.GetTriangleType("2147483647", "2147483647", "2147483647"), Is.EqualTo("Equilateral"));
+        }
+
+        [Test]
+        public void TestIsoceles()
+        {
+            Assert.That(_calculator.GetTriangleType("3", "3", "1"), Is.EqualTo("Isoceles"));
+            Assert.That(_calculator.GetTriangleType("2147483647", "2147483647", "1"), Is.EqualTo("Isoceles"));
+            Assert.That(_calculator.GetTriangleType("1","2147483647", "2147483647"), Is.EqualTo("Isoceles"));
+            Assert.That(_calculator.GetTriangleType("2147483647","1", "2147483647"), Is.EqualTo("Isoceles"));
+        }
+
+        [Test]
+        public void TestScalene()
+        {
+            Assert.That(_calculator.GetTriangleType("5", "4", "8"), Is.EqualTo("Scalene"));
+            Assert.That(_calculator.GetTriangleType("2147483647", "2147483647", "1"), Is.EqualTo("Isoceles"));
+            Assert.That(_calculator.GetTriangleType("1", "2147483647", "2147483647"), Is.EqualTo("Isoceles"));
+            Assert.That(_calculator.GetTriangleType("2147483647", "1", "2147483647"), Is.EqualTo("Isoceles"));
+        }
+
+        
+    }
+}
---
+++ Diana.Choksey/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TriangleTyperApp
+{
+    public class TriangleTypeCalculator
+    {
+        //private decimal _nbrA;
+        //private decimal _nbrB;
+        //private decimal _nbrC;
+    
+        public string GetTriangleType(string sideA, string sideB, string sideC)
+        {
+            int A;
+            if (!int.TryParse(sideA, out A))
+            {
+                return "Make sure all sides are Integers";
+            }
+            int B;
+            if (!int.TryParse(sideA, out B))
+            {
+                return "Make sure all sides are Integers";
+            }
+            int C;
+            if (!int.TryParse(sideA, out C))
+            {
+                return "Make sure all sides are Integers";
+            }
+
+            //make sure all inputs make a triangle
+            if ((A + B < C) || (A + C < B) || (B + C < A))
+
+                return "Sorry Dave these sides do not a triangle make";
+
+
+            if ((sideA == sideB) && (sideB == sideC))
+            {
+                return "Equilateral";      
+            }
+
+            if ((sideA == sideB) || (sideA == sideC) || (sideB == sideC))
+            {
+                return "Isoceles";
+            }
+
+            if ((sideA != sideB) && (sideA != sideC) && (sideB != sideC))
+            {
+                return "Scalene";
+            }
+            return "Stop messing around";
+        }
+    }
+}
+
