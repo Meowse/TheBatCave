03ae47b7a057ae8b6c922470e5d1ba58dd00402b
kenli6 <kenli6@remove.gmail.remove.com>
2014-02-11 21:03:34 -0800

    Homework 4 with Evil Tests

---
+++ Kenneth.Li/Homework/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/EvilTriangleTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using TriangleTyperApp;
+
+namespace TriangleTypeCalculatorTest
+{
+    [TestFixture]
+    public class EvilTriangleTests
+    {
+        private readonly TriangleTypeCalculator _calculator = new TriangleTypeCalculator();
+        private readonly string _maxInt = int.MaxValue.ToString();
+        private readonly string _maxIntMinus1 = (int.MaxValue - 1).ToString();
+        private readonly string _maxIntMinus2 = (int.MaxValue - 2).ToString();
+        
+        [Test]
+        public void VeryLongSidesEquilateral()
+        {
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxInt, _maxInt, _maxInt), Is.EqualTo("Equilateral"));
+        }
+
+        [Test]
+        public void VeryLongSidesIsosceles()
+        {
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal("1", _maxInt, _maxInt), Is.EqualTo("Isosceles"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxInt, _maxInt, "1"), Is.EqualTo("Isosceles"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxInt, "1", _maxInt), Is.EqualTo("Isosceles"));
+
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxIntMinus1, _maxInt, _maxInt), Is.EqualTo("Isosceles"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxInt, _maxInt, _maxIntMinus1), Is.EqualTo("Isosceles"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxInt, _maxIntMinus1, _maxInt), Is.EqualTo("Isosceles"));
+
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxIntMinus1, _maxIntMinus1, _maxInt), Is.EqualTo("Isosceles"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxIntMinus1, _maxInt, _maxIntMinus1), Is.EqualTo("Isosceles"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxInt, _maxIntMinus1, _maxIntMinus1), Is.EqualTo("Isosceles"));
+        }
+
+        [Test]
+        public void VeryLongSidesScalene()
+        {
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxInt, _maxIntMinus1, _maxIntMinus2), Is.EqualTo("Scalene"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxInt, _maxIntMinus2, _maxIntMinus1), Is.EqualTo("Scalene"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxIntMinus1, _maxInt, _maxIntMinus2), Is.EqualTo("Scalene"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxIntMinus1, _maxIntMinus2, _maxInt), Is.EqualTo("Scalene"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxIntMinus2, _maxInt, _maxIntMinus1), Is.EqualTo("Scalene"));
+            Assert.That(_calculator.ConvertTriangleValuesToDecimal(_maxIntMinus2, _maxIntMinus1, _maxInt), Is.EqualTo("Scalene"));
+        }
+    }
+}
---
+++ Kenneth.Li/Homework/Session 4/TriangleTyperApp/TriangleTypeCalculatorTest/NonTriangleTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using TriangleTyperApp;
+
+namespace TriangleTypeCalculatorTest
+{
+    [TestFixture]
+    class NonTriangleTests
+    {
+        private readonly TriangleTypeCalculator _calculator = new TriangleTypeCalculator();
+
+        [Test]
+        public void NonNumbersFail()
+        {
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("three", "four", "five")), Is.True);            
+        }
+
+        [Test]
+        public void NegativeNumbersFail()
+        {
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("-3", "-4", "-5")), Is.True);
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("-3", "4", "5")), Is.True);
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("3", "-4", "5")), Is.True);
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("3", "4", "-5")), Is.True);
+        }
+
+        [Test]
+        public void SidesCannotHaveZeroLength()
+        {
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("0", "0", "0")), Is.True);
+        }
+
+        [Test]
+        public void SidesMustMakeTriangle()
+        {
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("1", "7", "1")), Is.True);
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("7", "1", "1")), Is.True);
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("1", "1", "7")), Is.True);
+        }
+
+        [Test]
+        public void StraightLineIsNotTriangle()
+        {
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("1", "2", "1")), Is.True);
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("2", "1", "1")), Is.True);
+            Assert.That(IsError(_calculator.ConvertTriangleValuesToDecimal("1", "1", "2")), Is.True);
+        }
+
+        private bool IsError(string result)
+        {
+            return ((result != "Equilateral") && (result != "Isosceles") && (result != "Scalene"));
+        }
+    }
+}
