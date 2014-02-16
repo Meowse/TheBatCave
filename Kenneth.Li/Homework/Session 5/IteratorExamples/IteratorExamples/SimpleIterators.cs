5bbd80f2b92141768ba6cfa8f7f2d375efd60913
kenli6 <kenli6@remove.gmail.remove.com>
2014-02-15 18:58:51 -0800

    Homework 5 - Completed

--- Kenneth.Li/Homework/Session 5/IteratorExamples/IteratorExamples/SimpleIterators.cs
+++ Kenneth.Li/Homework/Session 5/IteratorExamples/IteratorExamples/SimpleIterators.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
+using System.Globalization;
+using System.Linq;
 
 namespace IteratorExamples
 {
@@ -14,17 +15,36 @@ namespace IteratorExamples
         // TODO: Re-implement this using only string arrays.
         public string[] EveryOtherElement(string[] input)
         {
-            List<string> result = new List<string>();
-            for (int i = 0; i < input.Length; i += 2)
+            int length = 0;
+            int oddLengths = ((input.Length)/2) +1;
+            int evenLengths = ((input.Length)/2);
+
+            if ((input.Length %2) == 0)
             {
-                result.Add(input[i]);
+                length = evenLengths;
             }
-            return result.ToArray();
+            if ((input.Length % 2) == 1)
+            {
+                length = oddLengths;
+            }
+
+            string[] result = new string[length];
+            for (int i = 0, j = 0; i < length; i++, j+=2 )
+            {
+                result[i] = input[j];
+            }
+            return result;
+//            List<string> result = new List<string>();
+//            for (int i = 0; i < input.Length; i += 2)
+//            {
+//                result.Add(input[i]);
+//            }
+//            return result.ToArray();
         }
 
         public int[] CountToWithWhileLoop(int max)
         {
-            int [] result = new int[max];
+            int[] result = new int[max];
             int i = 0;
             while (i < max)
             {
@@ -36,7 +56,7 @@ namespace IteratorExamples
 
         public int[] CountToWithForLoop(int max)
         {
-            int [] result = new int[max];
+            int[] result = new int[max];
             //for (int i = 0; i < max; i = i + 1)
             //for (int i = 0; i < max; i += 1)
             for (int i = 0; i < max; i++)
@@ -61,22 +81,49 @@ namespace IteratorExamples
 
         public int[] CountFromToWithForLoop(int min, int max)
         {
-            return new[] { 3, 4, 5, 6, 7 };
+            int length = max - min + 1;
+            int[] result = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = (min += 1) - 1;
+            }
+            return result;
+            //return new[] { 3, 4, 5, 6, 7 };
         }
 
-        public int[] CountFromToByWithForLoop(int p0, int p1, int p2)
+        public int[] CountFromToByWithForLoop(int min, int max, int increment)
         {
-            throw new NotImplementedException();
+            int length = ((max - min) / increment) + 1;
+            int[] result = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = (min += increment) - increment;
+            }
+            return result;;
         }
 
-        public int[] CountFromToByWithWhileLoop(int p0, int p1, int p2)
+        public int[] CountFromToByWithWhileLoop(int min, int max, int increment)
         {
-            throw new NotImplementedException();
+            int length = ((max - min) / increment) + 1;
+            int[] result = new int[length];
+            int i = 0;
+            while (i < length)
+            {
+                result[i] = (min += increment) - increment;
+                i = i + 1;
+            }
+            return result;
         }
 
-        public int[] BackFromBy(int i, int i1)
+        public int[] BackFromBy(int max, int min)
         {
-            throw new NotImplementedException();
+            int length = (max/min)+1;
+            int[] result = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = (max-=min)+min;
+            }
+            return result;
         }
     }
 }
