4583856874923115c0b0162e29b9eb0f95033eb6
cbrenn <cararbrennan@gmail.com>
2014-02-17 13:12:15 -0800

    Brennan HW 4 updates

--- Cara.Brennan/Week 4/Brennan HW 4 TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
+++ Cara.Brennan/Week 4/Brennan HW 4 TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -1,26 +1,63 @@
-﻿using System.Collections.Concurrent;
-using System.Windows.Forms;
+﻿using System;
 
 namespace TriangleTyperApp
 {
+
+
     public class TriangleTypeCalculator
     {
+
+        public static void Main()
+        {
+            Convert("sideA");
+            Convert("sideB");
+            Convert("sideC");
+        }
+
         public string GetTriangleType(string sideA, string sideB, string sideC)
         {
-            if sideA||sideB and sideB || sideC
-            return "Equilateral";
+            try
+            {
+                int prA = TriangleTypeCalclator.Parse(sideA);
+                int prB = TriangleTypeCalclator.Parse(sideB);
+                int prC = TriangleTypeCalclator.Parse(sideC);
 
-            else if sideA ||sideB or sideB||sideC or sideA||sideC
-            return "Isosceles"
-            
-            else
-            return "Scalene"
+            }
+            catch (FormatException)
+            {
 
-            if sideA or sideb or sidec is Not and integer
-            return "Please input integers."
-            
-            if ((sideA + sideB)< sideC) or ((sideB+sideC)<sideA ) or ((sideC+sideA)<sideB)
-             return "Sorry, your triangle is actually a T-Rex. It cannot live in the Triassic Period, please try again."
+                return "Your inputs are not acceptable integers, please try again. ";
+
+            }
+            return CheckforTriangle();
         }
+
+        public string CheckforTriangle()
+        {
+
+            if (prA <= 0 || prB <= 0 || prC <= 0)
+            {
+                return "Input must be a positive number";
+            }
+            if (prC <= (prA + prB) || prA <= (prB + prC) || prB <= (prA + prC))
+            {
+                return "I'm sorry your triangle is actually a T-Rex and cannot live in the Triassic Period. Please input new integers.";
+            }
+            if (prA == prB && prB == prC)
+            {
+                return "Equilateral";
+            }
+            if (prA == prB || prB == prC || prA == prC)
+            {
+                return "Isosceles";
+            }
+            return "Scalene";
+        }
+
+        public object prC { get; set; }
+    }
+
+    public class TriangleTypeCalclator
+    {
     }
 }
