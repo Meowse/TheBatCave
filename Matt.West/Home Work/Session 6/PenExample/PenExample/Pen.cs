eb107a55440eaee99ccdd44972faeba78bf95f00
MatthewSWest <MatWes@Microsoft.com>
2014-02-21 13:15:13 -0800

    Homework 6 - Baseline

---
+++ Matt.West/Home Work/Session 6/PenExample/PenExample/Pen.cs
@@ -0,0 +1,20 @@
+﻿namespace PenExample
+{
+    public class Pen
+    {
+        protected int DryingTimeInMinutes { get; set; }
+
+        public bool Capped { get; set; }
+
+        public void MinutesPass(int minutes)
+        {
+            // TODO: Age your pen here.
+            throw new System.NotImplementedException();
+        }
+
+        public void Write(string something)
+        {
+            // TODO: Optionally age your pen here based on time and ink consumption.
+        }
+    }
+}
