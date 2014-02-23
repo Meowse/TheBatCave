f61e8b11a7fc914f67f80add341cdedf43eacdf9
dulcinealedgerwood <dulcinealedgerwood>
2014-02-23 10:08:55 -0800

    Homework 4 final
    Finally done with Homework 4.

--- Dulcinea.Ledgerwood/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
+++ Dulcinea.Ledgerwood/TriangleTyperApp/TriangleTypeCalculatorTest/BasicCalculatorTests.cs
@@ -7,11 +7,28 @@ namespace TriangleTypeCalculatorTest
     public class BasicCalculatorTests
     {
         private readonly TriangleTypeCalculator _calculator = new TriangleTypeCalculator();
+        private const string Equilateral = "Equilateral";
+        private const string Scalene = "Scalene";
+        private const string Isosceles = "Isosceles";
 
         [Test]
         public void TestEquilateral()
         {
-            Assert.That(_calculator.GetTriangleType("3", "3", "3"), Is.EqualTo("Equilateral"));
+            Assert.That(_calculator.GetTriangleType("3", "3", "3"), Is.EqualTo(Equilateral));
+            Assert.That(_calculator.GetTriangleType("3.0", "3", "3"), Is.EqualTo("Inputs must be a positive integer"));
+            Assert.That(_calculator.GetTriangleType(int.MaxValue.ToString(), int.MaxValue.ToString(), int.MaxValue.ToString()), Is.EqualTo(Equilateral));
+        }
+
+        [Test]
+        public void TestScalene()
+        {
+            Assert.That(_calculator.GetTriangleType("3", "4", "5"), Is.EqualTo(Scalene));
+        }
+
+        [Test]
+        public void TestIsosceles()
+        {
+            Assert.That(_calculator.GetTriangleType("3", "4", "4"), Is.EqualTo(Isosceles));
         }
     }
 }
--- Dulcinea.Ledgerwood/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
+++ Dulcinea.Ledgerwood/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -1,10 +1,54 @@
-﻿namespace TriangleTyperApp
+﻿using System;
+
+namespace TriangleTyperApp
 {
     public class TriangleTypeCalculator
     {
+        public int SideA = 0;
+        public int SideB = 0;
+        public int SideC = 0;
+
+        private const string PositiveIntOnly = "Inputs must be a positive integer";
+        private const string NotTriangle = "Not a triangle";
+
+
         public string GetTriangleType(string sideA, string sideB, string sideC)
         {
-            return "Equilateral";
+            if (!Int32.TryParse(sideA, out SideA))
+            {
+                return PositiveIntOnly;
+            }
+            if (!Int32.TryParse(sideB, out SideB))
+            {
+                return PositiveIntOnly;
+            }
+            if (!Int32.TryParse(sideC, out SideC))
+            {
+                return PositiveIntOnly;
+            }
+            return FindTriangleType();
+        }
+
+        public string FindTriangleType()
+        {
+     
+            if (SideA <= 0 || SideB <= 0 || SideC <= 0)
+            {
+                return PositiveIntOnly;
+            }
+            if (SideC <= Math.Abs(SideA - SideB)  ||  SideA <= Math.Abs(SideB - SideC)  || SideB  <= Math.Abs(SideA - SideC))
+            {
+                return NotTriangle;
+            }
+            if (SideA == SideB && SideB == SideC)
+            {
+                return "Equilateral";
+            }
+            if (SideA == SideB || SideB == SideC || SideA == SideC)
+            {
+                return "Isosceles";
+            }
+            return "Scalene";
         }
     }
 }
