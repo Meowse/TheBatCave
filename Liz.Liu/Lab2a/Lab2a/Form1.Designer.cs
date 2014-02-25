9750723eb4ba1f8dd82affd597cf5f93d988124d
Liz Liu <elizabeth.an.liu@gmail.com>
2014-02-25 13:52:04 -0800

    Homework

---
+++ Liz.Liu/Lab2a/Lab2a/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Lab2a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.output2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.output3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.output4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.output5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.output6 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.output7 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // output1
+            // 
+            this.output1.AutoSize = true;
+            this.output1.Location = new System.Drawing.Point(200, 17);
+            this.output1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.output1.Name = "output1";
+            this.output1.Size = new System.Drawing.Size(192, 26);
+            this.output1.TabIndex = 3;
+            this.output1.Text = "output 1 goes here";
+            this.output1.Click += new System.EventHandler(this.output1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hello world";
+            // 
+            // output2
+            // 
+            this.output2.AutoSize = true;
+            this.output2.Location = new System.Drawing.Point(200, 63);
+            this.output2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.output2.Name = "output2";
+            this.output2.Size = new System.Drawing.Size(192, 26);
+            this.output2.TabIndex = 5;
+            this.output2.Text = "output 2 goes here";
+            this.output2.Click += new System.EventHandler(this.output2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "one two three";
+            // 
+            // output3
+            // 
+            this.output3.AutoSize = true;
+            this.output3.Location = new System.Drawing.Point(200, 110);
+            this.output3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.output3.Name = "output3";
+            this.output3.Size = new System.Drawing.Size(192, 26);
+            this.output3.TabIndex = 7;
+            this.output3.Text = "output 3 goes here";
+            this.output3.Click += new System.EventHandler(this.output3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 112);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 26);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "45";
+            // 
+            // output4
+            // 
+            this.output4.AutoSize = true;
+            this.output4.Location = new System.Drawing.Point(200, 156);
+            this.output4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.output4.Name = "output4";
+            this.output4.Size = new System.Drawing.Size(192, 26);
+            this.output4.TabIndex = 9;
+            this.output4.Text = "output 4 goes here";
+            this.output4.Click += new System.EventHandler(this.output4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 158);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 26);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "\"\"\"";
+            // 
+            // output5
+            // 
+            this.output5.AutoSize = true;
+            this.output5.Location = new System.Drawing.Point(200, 202);
+            this.output5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.output5.Name = "output5";
+            this.output5.Size = new System.Drawing.Size(192, 26);
+            this.output5.TabIndex = 11;
+            this.output5.Text = "output 5 goes here";
+            this.output5.Click += new System.EventHandler(this.output5_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(24, 204);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 26);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "\\\\\\\\";
+            // 
+            // output6
+            // 
+            this.output6.AutoSize = true;
+            this.output6.Location = new System.Drawing.Point(200, 250);
+            this.output6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.output6.Name = "output6";
+            this.output6.Size = new System.Drawing.Size(192, 26);
+            this.output6.TabIndex = 13;
+            this.output6.Text = "output 6 goes here";
+            this.output6.Click += new System.EventHandler(this.output6_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(24, 252);
+            this.label11.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 26);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "\"\\\"\"";
+            // 
+            // output7
+            // 
+            this.output7.AutoSize = true;
+            this.output7.Location = new System.Drawing.Point(200, 296);
+            this.output7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.output7.Name = "output7";
+            this.output7.Size = new System.Drawing.Size(192, 26);
+            this.output7.TabIndex = 15;
+            this.output7.Text = "output 7 goes here";
+            this.output7.Click += new System.EventHandler(this.output7_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(24, 298);
+            this.label13.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(98, 26);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "\"\\\"\"\"\\\\\\\"\\\"\"";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(404, 63);
+            this.label16.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(212, 26);
+            this.label16.TabIndex = 18;
+            this.label16.Text = "(using string.Format)";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(404, 110);
+            this.label17.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(260, 26);
+            this.label17.TabIndex = 19;
+            this.label17.Text = "(using some int variables)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 469);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.output7);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.output6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.output5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.output4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.output3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.output2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.output1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label output1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label output2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label output3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label output4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label output5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label output6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label output7;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+    }
+}
+
