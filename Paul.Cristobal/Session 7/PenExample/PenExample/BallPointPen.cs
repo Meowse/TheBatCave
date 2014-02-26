9d76d0e0be90cc8d9b26ebcba72158240472df1a
paul206 <paul206@uw.edu>
2014-02-25 20:57:07 -0800

    PenExample Check in

---
+++ Paul.Cristobal/Session 7/PenExample/PenExample/BallPointPen.cs
@@ -0,0 +1,10 @@
+﻿namespace PenExample
+{
+    public class BallPointPen : Pen
+    {
+        public BallPointPen(int priceInDollars)
+        {
+            DryingTimeInMinutes = priceInDollars*24*60;
+        }
+    }
+}
