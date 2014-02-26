bb23cb89ab80c4ba0bd378f390edaee8a4c13c0a
kenli6 <kenli6@remove.gmail.remove.com>
2014-02-25 21:03:36 -0800

    PenExample - Baseline

---
+++ Kenneth.Li/Homework/PenExample/PenExample/FeltTipPen.cs
@@ -0,0 +1,12 @@
+﻿namespace PenExample
+{
+    public class FeltTipPen : Pen
+    {
+        public FeltTipPen()
+        {
+            // TODO: Figure out the right way to get rid of the comment
+            // on the next line.
+            DryingTimeInMinutes = 60*5; // 5 hours
+        }
+    }
+}
