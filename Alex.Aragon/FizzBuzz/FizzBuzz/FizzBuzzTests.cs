88b06b2c0db12e53504f7c50cfd0dc838019a86c
Alex Aragon <aragonwa@gmail.com>
2014-02-25 19:26:55 -0800

    FizzBuzz attempt

---
+++ Alex.Aragon/FizzBuzz/FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IteratorExamples;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        private static readonly SimpleIterators SimpleIterators = new SimpleIterators();
+
+        [Test]
+        public void CountThree()
+        {
+            String[] expected = { "1", "2", "3fizz" };
+            Assert.That(SimpleIterators.CountThree(), Is.EqualTo(expected));
+        }
+
+       
+
+
+    }
+}
