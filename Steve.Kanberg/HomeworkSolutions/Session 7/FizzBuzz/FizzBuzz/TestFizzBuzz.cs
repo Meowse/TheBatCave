1ecf65ab30af1733ec546d390b3ef964bff71aba
vstevka <vstevka@hotmail.com>
2014-02-26 00:48:35 -0800

    FizzBuzz Checkin #1

---
+++ Steve.Kanberg/HomeworkSolutions/Session 7/FizzBuzz/FizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    class TestFizzBuzz
+    {
+        [Test]
+        public void IsFizzBuzz()
+        {
+            string expected = "Fizz";
+            Assert.That(FizzBuzzCompiler.FizzBuzzed("Fizz"), Is.EqualTo(expected));
+
+            string expected2 = "Buzz";
+            Assert.That(FizzBuzzCompiler.FizzBuzzed("Buzz"), Is.EqualTo(expected2));
+
+            string expected3 = "FizzBuzz";
+            Assert.That(FizzBuzzCompiler.FizzBuzzed("FizzBuzz"), Is.EqualTo(expected3));
+        }
+    }
+}
