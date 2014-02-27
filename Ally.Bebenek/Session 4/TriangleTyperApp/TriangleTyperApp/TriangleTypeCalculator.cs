de1153f3ea3d4ff31bf09d12f387e4d045110c35
AllyBeBe <aleksandrajedrak@gmail.com>
2014-02-26 21:12:33 -0800

    HW4
    challenge mode - 2 tests

---
+++ Ally.Bebenek/Session 4/TriangleTyperApp/TriangleTyperApp/TriangleTypeCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace TriangleTyperApp
+{
+    public class TriangleTypeCalculator
+    {
+        public string GetTriangleType(string sideA, string sideB, string sideC)
+        {
+            return "Equilateral";
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public bool AreEqual(int a, int b)
+        {
+            if (a == b)
+                return true;
+            return false;
+        }
+    }
+}
