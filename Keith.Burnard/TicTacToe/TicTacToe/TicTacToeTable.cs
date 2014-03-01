6acf1b17dd5a4d53f4bbb54979fa8f4e9385a3ff
keibur <captainkeibur@gmail.com>
2014-03-01 14:15:15 -0800

    Horizontal / Vertical winner

--- Keith.Burnard/TicTacToe/TicTacToe/TicTacToeTable.cs
+++ Keith.Burnard/TicTacToe/TicTacToe/TicTacToeTable.cs
@@ -8,5 +8,80 @@ namespace TicTacToe
 {
     class TicTacToeTable
     {
+        private const int Player1 = 1;
+        private const int Player2 = 2;
+
+        public bool PlayerOneWins { get; private set; }
+        public bool PlayerTwoWins { get; private set; }
+
+        int[,] _ticTacToeTable = new int[3,3];
+
+        public void NewGame()
+        {
+            _ticTacToeTable = new int[3, 3];
+            PlayerOneWins = false;
+            PlayerTwoWins = false;
+        }
+
+        public int MakeAMove(int row, int column, int player)
+        {
+            _ticTacToeTable[row, column] = player;
+
+            CheckForAWinner();
+
+            if (PlayerOneWins)
+            {
+                return Player1;
+            }
+            return PlayerTwoWins ? Player2 : 0;
+        }
+
+        private void CheckForAWinner()
+        {
+            CheckForHorizontalWinner();
+            CheckForVerticalWinner();
+            //CheckForDiagonalWinner();
+        }
+
+        private void CheckForDiagonalWinner()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CheckForVerticalWinner()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (_ticTacToeTable[0, i] == Player1 && _ticTacToeTable[1, i] == Player1 && _ticTacToeTable[2, i] == Player1)
+                {
+                    PlayerOneWins = true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (_ticTacToeTable[0,i] == Player2 && _ticTacToeTable[1, i] == Player2 && _ticTacToeTable[2, i] == Player2)
+                {
+                    PlayerTwoWins = true;
+                }
+            }
+        }
+
+        private void CheckForHorizontalWinner()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (_ticTacToeTable[i, 0] == Player1 && _ticTacToeTable[i, 1] == Player1 && _ticTacToeTable[i, 2] == Player1)
+                {
+                    PlayerOneWins = true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (_ticTacToeTable[i, 0] == Player2 && _ticTacToeTable[i, 1] == Player2 && _ticTacToeTable[i, 2] == Player2)
+                {
+                    PlayerTwoWins = true;
+                }
+            }
+        }
     }
 }
--- Keith.Burnard/TicTacToe/TicTacToe/TicTacToeTableTest.cs
+++ Keith.Burnard/TicTacToe/TicTacToe/TicTacToeTableTest.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace TicTacToe
 {
     class TicTacToeTableTest
     {
+        private static readonly TicTacToeTable _TicTacToeTable = new TicTacToeTable();
+
+        [Test]
+        public void TestHorizontalWin()
+        {
+            Assert.That(_TicTacToeTable.MakeAMove(0, 0, 1), Is.EqualTo(0));
+            Assert.That(_TicTacToeTable.MakeAMove(0, 1, 1), Is.EqualTo(0));
+            Assert.That(_TicTacToeTable.MakeAMove(0, 2, 1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TestVerticalWin()
+        {
+            _TicTacToeTable.NewGame();
+            Assert.That(_TicTacToeTable.MakeAMove(0, 0, 1), Is.EqualTo(0));
+            Assert.That(_TicTacToeTable.MakeAMove(1, 0, 1), Is.EqualTo(0));
+            Assert.That(_TicTacToeTable.MakeAMove(2, 0, 1), Is.EqualTo(1));
+        }
     }
 }
