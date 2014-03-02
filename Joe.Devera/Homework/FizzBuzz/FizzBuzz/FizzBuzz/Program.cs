783de966f610ecfc04481f5d195ed4204f73ce1b
kujoe13 <kujoe13@gmail.com>
2014-03-02 08:15:42 -0800

    FizzBuzz No Tests

---
+++ Joe.Devera/Homework/FizzBuzz/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            string fizz = "fizz";
+            string buzz = "buzz";
+
+            for (int i = 100; i > 0; i--)
+            {
+                if (i%3 == 0)
+                {
+                    Console.WriteLine(fizz);
+                }
+                else if (i%5 == 0)
+                {
+                    Console.WriteLine(buzz);
+                }
+                else if ((i%3 == 0) && (i%5 == 0))
+                {
+                    Console.WriteLine(fizz+buzz);
+                }
+                else
+                {
+                    Console.WriteLine(i); 
+                }          
+            }
+        }
+    }
+}
