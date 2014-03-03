c352759d25771dc8196c00a120cfb2dc6f8fdf7c
MatthewSWest <MatWes@Microsoft.com>
2014-03-02 16:43:35 -0800

    Count back from 100 to 0 by 7 (first and final)

---
+++ Matt.West/Home Work/Session 6/Count down from 100 to 0 by 7/ConsoleAppCountFrom100to0by7.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountFrom100by7
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+//Expected: 100, 93, 86 … 0
+//
+//Actual: 93, 86, 79 … 0, -5
+//
+//Error type: “off-by-one error”, aka “fencepost error”
+//
+//numbers between 15 and 13
+//
+//x - y + 1
+
+            for (int i = 100; i > 0; i = i-7)
+            {
+
+                Console.WriteLine(i);
+
+            }
+            Console.WriteLine(0);
+            Console.ReadLine();
+
+        }
+    }
+}
