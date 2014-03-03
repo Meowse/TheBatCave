3d838aa5dc28107811d9b07e168e75c4564e3f7c
MatthewSWest <MatWes@Microsoft.com>
2014-03-02 16:54:45 -0800

    Fizz Buzz - Baseline Checkin

---
+++ Matt.West/Home Work/FizzBuzz/Fizz Buzz/Fizz Buzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//i. FizzBuzz: print out the numbers from 1 to N, one per line. BUT:
+//ii. If a number is divisible by 3, print out “Fizz” instead of the number. AND:
+//iii. If a number is divisible by 5, print out “Buzz” instead of the number. 
+//EXCEPT:
+//iv. If a number is divisible by both 3 and 5, print out “FizzBuzz” instead of the number.
+//
+//v. And -- create a brand new Visual Studio solution, including a separate project to hold your tests.
+
+
+
+
+        }
+    }
+}
