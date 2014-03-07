52014760614adf68d37624f31613a57e88f1fbd1
keibur <captainkeibur@gmail.com>
2014-03-07 15:14:49 -0800

    added inputs for devisors and strings

---
+++ Keith.Burnard/FizzBuzzPlus/FizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    class TestFizzBuzz
+    {
+        FizzBuzzCounter _fizzBuzzCounter = new FizzBuzzCounter();
+
+        [Test]
+        public void CountFromZeroToTen()
+        {
+            string[] expected = { "", "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8","Fizz","Buzz" };
+            Assert.That(_fizzBuzzCounter.Counter(10, 3, "Fizz", 5, "Buzz"), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CountFromZeroToFifteen()
+        {
+            string[] expected = { "", "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13","14","FizzBuzz" };
+            Assert.That(_fizzBuzzCounter.Counter(15, 3, "Fizz", 5, "Buzz"), Is.EqualTo(expected));
+        }
+    }
+}
