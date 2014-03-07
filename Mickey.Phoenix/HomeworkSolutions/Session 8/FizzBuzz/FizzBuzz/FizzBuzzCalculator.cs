77a9904f83b2f5c05067e601246ba7f3010dee1d
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-03-06 21:03:42 -0800

    After adding FizzBuzzCalculator

---
+++ Mickey.Phoenix/HomeworkSolutions/Session 8/FizzBuzz/FizzBuzz/FizzBuzzCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzCalculator
+    {
+        public string Calculate(int i)
+        {
+            if (i == 3)
+            {
+                return "Fizz";
+            }
+            return i.ToString();
+        }
+    }
+}
--- Mickey.Phoenix/HomeworkSolutions/Session 8/FizzBuzz/FizzBuzz/Form1.cs
+++ Mickey.Phoenix/HomeworkSolutions/Session 8/FizzBuzz/FizzBuzz/Form1.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FizzBuzz
 {
     public partial class Form1 : Form
     {
+        private readonly FizzBuzzCalculator _fizzBuzzCalculator = new FizzBuzzCalculator();
+
         public Form1()
         {
             InitializeComponent();
@@ -23,14 +18,14 @@ namespace FizzBuzz
 
             if (maxValue < 1)
             {
-                outputLabel.Text = "Please enter a positive integer.";
+                outputLabel.Text = @"Please enter a positive integer.";
                 return;
             }
 
-            outputLabel.Text = "Calculating FizzBuzz from 1 to " + maxValue;
+            outputLabel.Text = @"Calculating FizzBuzz from 1 to " + maxValue;
             for (int i = 1; i <= maxValue; i++)
             {
-                outputLabel.Text = outputLabel.Text + Environment.NewLine + i;
+                outputLabel.Text = outputLabel.Text + Environment.NewLine + _fizzBuzzCalculator.Calculate(i);
             }
         }
 
