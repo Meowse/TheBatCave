a888bd6f9ae5d100bb64433305bd91e9ee32614a
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-03-06 21:26:08 -0800

    FizzBuzz complete.

--- Mickey.Phoenix/HomeworkSolutions/Session 8/FizzBuzz/FizzBuzz/FizzBuzzCalculator.cs
+++ Mickey.Phoenix/HomeworkSolutions/Session 8/FizzBuzz/FizzBuzz/FizzBuzzCalculator.cs
@@ -1,12 +1,22 @@
-﻿namespace FizzBuzz
+﻿using System.Windows.Forms.VisualStyles;
+
+namespace FizzBuzz
 {
     public class FizzBuzzCalculator
     {
         public string Calculate(int i)
         {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
             if (i % 3 == 0)
             {
                 return "Fizz";
+            }
+            if (i % 5 == 0)
+            {
+                return "Buzz";
             }
             return i.ToString();
         }
--- Mickey.Phoenix/HomeworkSolutions/Session 8/FizzBuzz/FizzBuzz/FizzBuzzCalculatorTests.cs
+++ Mickey.Phoenix/HomeworkSolutions/Session 8/FizzBuzz/FizzBuzz/FizzBuzzCalculatorTests.cs
@@ -8,11 +8,32 @@ namespace FizzBuzz
         private readonly FizzBuzzCalculator _calculator = new FizzBuzzCalculator();
 
         [Test]
-        public void ReturnsFizzForMultiplesOf3()
+        public void ReturnsFizzForMultiplesOf3ThatAreNotMultiplesOf5()
         {
-            Assert.That(_calculator.Calculate(3), Is.EqualTo("Fizz"));
-            Assert.That(_calculator.Calculate(6), Is.EqualTo("Fizz"));
-            Assert.That(_calculator.Calculate(9), Is.EqualTo("Fizz"));
+            Assert.That(_calculator.Calculate(1 * 3), Is.EqualTo("Fizz"));
+            Assert.That(_calculator.Calculate(2 * 3), Is.EqualTo("Fizz"));
+            Assert.That(_calculator.Calculate(3 * 3), Is.EqualTo("Fizz"));
+            Assert.That(_calculator.Calculate(4 * 3), Is.EqualTo("Fizz"));
+            // Skipping "5 * 3", since it shouldn't return "Fizz" for that
+            Assert.That(_calculator.Calculate(6 * 3), Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void ReturnsBuzzForMultiplesOf5ThatAreNotMultiplesOf3()
+        {
+            Assert.That(_calculator.Calculate(1 * 5), Is.EqualTo("Buzz"));
+            Assert.That(_calculator.Calculate(2 * 5), Is.EqualTo("Buzz"));
+            // Skipping "3 * 5", since it shouldn't return "Buzz" for that
+            Assert.That(_calculator.Calculate(4 * 5), Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void ReturnsFizzBuzzForMultiplesOf3And5()
+        {
+            Assert.That(_calculator.Calculate(3*5), Is.EqualTo("FizzBuzz"));
+            Assert.That(_calculator.Calculate(2 * 3 * 5), Is.EqualTo("FizzBuzz"));
+            Assert.That(_calculator.Calculate(3 * 3 * 5), Is.EqualTo("FizzBuzz"));
+            Assert.That(_calculator.Calculate(5 * 3 * 5), Is.EqualTo("FizzBuzz"));
         }
     }
 }
