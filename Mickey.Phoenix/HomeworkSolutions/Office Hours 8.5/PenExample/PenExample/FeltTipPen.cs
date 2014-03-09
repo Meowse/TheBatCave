9d935d37cf01a4fd4929b069cbfdea3ca86eb40c
Mickey Phoenix <meowse@remove.gmail.remove.com>
2014-03-09 16:57:35 -0700

    Pen Example for Office Hours 8.5

---
+++ Mickey.Phoenix/HomeworkSolutions/Office Hours 8.5/PenExample/PenExample/FeltTipPen.cs
@@ -0,0 +1,14 @@
+﻿namespace PenExample
+{
+    public class FeltTipPen : Pen
+    {
+        public FeltTipPen()
+        {
+            Description = "Felt-tip Pen";
+
+            // TODO: Figure out the right way to get rid of the comment
+            // on the next line.
+            DryingTimeInMinutes = 60*5; // 5 hours
+        }
+    }
+}
