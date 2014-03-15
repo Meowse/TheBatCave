7fc173524d9ec17d24e4d55cd6d29e635ae44dc0
Meowse <meowse@remove.gmail.remove.com>
2014-03-15 11:17:50 -0700

    Office Hours 9.25 linked list example

---
+++ Mickey.Phoenix/Session 9.25 office hours/LinkedList/LinkedList/Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public Person Value { get; set; }
+        public Node Next { get; set; }
+        public int Count
+        {
+            get
+            {
+                if (Next == null)
+                {
+                    return 1;
+                }
+                return Next.Count + 1;
+            }
+        }
+
+        public Node DescendantAt(int i)
+        {
+            if (i == 0)
+            {
+                return this;
+            }
+            return Next.DescendantAt(i-1);
+        }
+    }
+}
