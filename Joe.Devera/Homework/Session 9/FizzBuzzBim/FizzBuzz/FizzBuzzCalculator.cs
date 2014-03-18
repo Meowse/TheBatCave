469d4cb66981d10774aaa744e55f8a068418b407
kujoe13 <kujoe13@gmail.com>
2014-03-18 11:16:53 -0700

    Attempt of FizzBuzzBIm

--- Joe.Devera/Homework/Session 9/FizzBuzzBim/FizzBuzz/FizzBuzzCalculator.cs
+++ Joe.Devera/Homework/Session 9/FizzBuzzBim/FizzBuzz/FizzBuzzCalculator.cs
@@ -4,22 +4,36 @@
     {
         private readonly int _fizzDivisor;
         private readonly int _buzzDivisor;
+        private readonly int _bimDivisor;
 
-        public FizzBuzzCalculator() : this(3, 5)
+        public FizzBuzzCalculator() : this(2, 3, 5)
         {
         }
 
-        public FizzBuzzCalculator(int fizzDivisor, int buzzDivisor)
+        public FizzBuzzCalculator(int fizzDivisor, int buzzDivisor, int bimDivisor)
         {
             _fizzDivisor = fizzDivisor;
             _buzzDivisor = buzzDivisor;
+            _bimDivisor = bimDivisor;
         }
 
         public string Calculate(int i)
         {
-            if (i % _fizzDivisor == 0 && i % _buzzDivisor == 0)
+            if (i % _fizzDivisor == 0 && i % _buzzDivisor == 0 && i % _bimDivisor == 0)
+            {
+                return "FizzBuzzBim";
+            }
+            if (i % _buzzDivisor == 0 && i % _bimDivisor == 0)
+            {
+                return "BuzzBim";
+            }
+            if (i%_fizzDivisor == 0 && i%_buzzDivisor == 0)
             {
                 return "FizzBuzz";
+            }
+            if (i % _fizzDivisor == 0 && i % _bimDivisor == 0)
+            {
+                return "FizzBim";
             }
             if (i % _fizzDivisor == 0)
             {
@@ -28,6 +42,10 @@
             if (i % _buzzDivisor == 0)
             {
                 return "Buzz";
+            }
+            if (i % _bimDivisor == 0)
+            {
+                return "Bim";
             }
             return i.ToString();
         }
