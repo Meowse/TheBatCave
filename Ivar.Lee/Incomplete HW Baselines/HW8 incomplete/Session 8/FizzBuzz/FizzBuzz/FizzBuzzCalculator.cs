e422e5a49307682a89dccaf323426d759c9e6ce3
ivarmlee11 <ivarr5@gmail.com>
2014-03-18 21:04:53 -0700

    Incomplete HW Baseline Check ins
    A collection of incomplete HW assignments and the baseline Fizzbuzz
    project.

---
+++ Ivar.Lee/Incomplete HW Baselines/HW8 incomplete/Session 8/FizzBuzz/FizzBuzz/FizzBuzzCalculator.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzzCalculator
+    {
+        private readonly int _fizzDivisor;
+        private readonly int _buzzDivisor;
+
+        public FizzBuzzCalculator() : this(3, 5)
+        {
+        }
+
+        public FizzBuzzCalculator(int fizzDivisor, int buzzDivisor)
+        {
+            _fizzDivisor = fizzDivisor;
+            _buzzDivisor = buzzDivisor;
+        }
+
+        public string Calculate(int i)
+        {
+            if (i % _fizzDivisor == 0 && i % _buzzDivisor == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (i % _fizzDivisor == 0)
+            {
+                return "Fizz";
+            }
+            if (i % _buzzDivisor == 0)
+            {
+                return "Buzz";
+            }
+            return i.ToString();
+        }
+    }
+}
