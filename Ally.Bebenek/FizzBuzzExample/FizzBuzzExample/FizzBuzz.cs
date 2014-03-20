d60972afb9ded0a0269a7bd72a0fb768564e128a
AllyBeBe <aleksandrajedrak@gmail.com>
2014-03-19 21:16:05 -0700

    HW7
    MyFizzBuzz&Tests

---
+++ Ally.Bebenek/FizzBuzzExample/FizzBuzzExample/FizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzExample
+{
+    public class FizzBuzz
+    {
+        public int[] WriteFizzBuzzByWithForLoop(int max)
+        {
+
+            //If /3, then "Fizz", if /5 then "Buzz", if /3 & /5, then "FizzBuzz"
+        }
+    }
+}
